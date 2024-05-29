1555258b9ad18cda32cf209c8e13440cc026b065
Trần Vĩnh Thiện Phúc <143604440+tvtphuc-axonivy@users.noreply.github.com>
2024-05-29 15:42:13 +0700

    MARP-275 Create new BE project (#4)
    * MARP-275 Create new BE project
    
    * User MockitoJUnitRunner instead of SpringRunner
    
    * RHT-275 Handle For SonaQube
    
    * MARP-275 Add lombok.config
    
    ---------

---
+++ src/main/java/com/axonivy/market/config/WebConfig.java
@@ -0,0 +1,22 @@
+package com.axonivy.market.config;
+
+import static com.axonivy.market.constants.RequestMappingConstants.USER_MAPPING;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+  private final HeaderInterceptor headerInterceptor;
+
+  public WebConfig(HeaderInterceptor headerInterceptor) {
+    this.headerInterceptor = headerInterceptor;
+  }
+
+  @Override
+  public void addInterceptors(InterceptorRegistry registry) {
+    registry.addInterceptor(headerInterceptor).addPathPatterns(USER_MAPPING);
+  }
+}
---
+++ src/main/java/com/axonivy/market/constants/RequestMappingConstants.java
@@ -0,0 +1,5 @@
+package com.axonivy.market.constants;
+
+public class RequestMappingConstants {
+  public static final String USER_MAPPING = "/user";
+}
