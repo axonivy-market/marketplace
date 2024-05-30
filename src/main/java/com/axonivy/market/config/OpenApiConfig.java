e8bc03725e0e9592c5857cd57b0a908eea302577
Trần Vĩnh Thiện Phúc <phuc.tranvinhthien@axonactive.com>
2024-05-30 14:52:12 +0700

    MARP-275 add specific type for pathItem

--- src/main/java/com/axonivy/market/config/OpenApiConfig.java
+++ src/main/java/com/axonivy/market/config/OpenApiConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import io.swagger.v3.oas.models.Operation;
+import io.swagger.v3.oas.models.PathItem;
 import io.swagger.v3.oas.models.media.StringSchema;
 import io.swagger.v3.oas.models.parameters.Parameter;
 
@@ -17,7 +18,7 @@ public class OpenApiConfig {
   }
 
   private OpenApiCustomizer customGlobalHeaders() {
-    return openApi -> openApi.getPaths().values().forEach(pathItem -> {
+    return openApi -> openApi.getPaths().values().forEach((PathItem pathItem) -> {
       for (Operation operation : pathItem.readOperations()) {
         Parameter headerParameter = new Parameter().in("header").schema(new StringSchema()).name("X-Requested-By")
             .description("ivy").required(true);
