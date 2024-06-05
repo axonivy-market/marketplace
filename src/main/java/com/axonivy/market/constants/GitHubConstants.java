a012ca66832098026d058a516ad7603c9bc08c4d
Dinh Nguyen <dinh.nguyentrinhquoc@axonactive.com>
2024-06-05 13:55:52 +0700

    Update common GitHub Service

---
+++ src/main/java/com/axonivy/market/constants/GitHubConstants.java
@@ -0,0 +1,9 @@
+package com.axonivy.market.constants;
+
+public class GitHubConstants {
+    public static final String AXON_IVY_MARKET_ORGANIZATION_NAME = "axonivy-market";
+    public static final String AXON_IVY_MARKET_PLACE_REPO_NAME = "market";
+    public static final String AXON_IVY_MARKET_PLACE_PATH = "market";
+
+
+}
