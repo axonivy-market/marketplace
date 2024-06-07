c217d031c3ec1df3e7f65025498ca90fe73412ad
nqhoan-axonivy <nqhoan@axonactive.com>
2024-06-07 18:10:53 +0700

    Fix jacoco report

--- src/main/java/com/axonivy/market/constants/DocumentConstants.java
+++ src/main/java/com/axonivy/market/constants/DocumentConstants.java
@@ -1,5 +1,9 @@
 package com.axonivy.market.constants;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class DocumentConstants {
   public static final String USER_DOCUMENT = "User";
 }
