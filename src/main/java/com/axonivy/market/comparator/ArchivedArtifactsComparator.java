f9940d7166307e73b091fd1054d8c90dc791079f
Dinh Nguyen <dinh.nguyentrinhquoc@axonactive.com>
2024-06-18 10:12:43 +0700

    Update ArchivedArtifactsComparator.java

--- src/main/java/com/axonivy/market/comparator/ArchivedArtifactsComparator.java
+++ src/main/java/com/axonivy/market/comparator/ArchivedArtifactsComparator.java
@@ -5,8 +5,8 @@ import com.axonivy.market.github.model.ArchivedArtifact;
 import java.util.Comparator;
 
 public class ArchivedArtifactsComparator implements Comparator<ArchivedArtifact> {
-    LatestVersionComparator comparator;
-
+    private final LatestVersionComparator comparator = new LatestVersionComparator();
+    
     @Override
     public int compare(ArchivedArtifact v1, ArchivedArtifact v2) {
         return comparator.compare(v1.getLastVersion(), v2.getLastVersion());
