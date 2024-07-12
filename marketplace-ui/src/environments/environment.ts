a9c8f26ba154c0f42479323951149f6df688d933
Hoan Nguyen <83745591+nqhoan-axonivy@users.noreply.github.com>
2024-07-12 10:20:53 +0700

    Fix dockercompose (#29)
    Co-authored-by: Hoan Nguyen <nqhoan@axonactive.com>

--- marketplace-ui/src/environments/environment.ts
+++ marketplace-ui/src/environments/environment.ts
@@ -1,7 +1,4 @@
 export const environment = {
   production: true,
-  //apiUrl: 'http://api-service-network:8080/marketplace-service'
-  //apiUrl: 'http://localhost:9090/marketplace-service'
-  //apiUrl: 'http://192.168.73.13:8080'
-  apiUrl: 'http://192.168.73.17:9090/marketplace-service'
+  apiUrl: 'http://marketplace.server.ivy-cloud.com:8080/marketplace-service'
 };
