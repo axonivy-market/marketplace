dbd9e830ed361d8191d29e87f6de23da6007d1f7
Hoan Nguyen <hoan.nguyen01@axonactive.com>
2024-07-18 11:18:26 +0700

    Unify code

--- marketplace-build/README.md
+++ marketplace-build/README.md
@@ -1,7 +1,7 @@
 # Get starts with Marketplace build
 
 ### Set up MongoDB with authentication mode
-* Navigate to ``marketplace-build/config/mongodb`` and execute the ``docker-compose up`` to start MongoDB with non-auth mode and create a root admin user.
+* Navigate to ``marketplace-build/config/mongodb`` and execute the ``docker-compose up -d`` to start MongoDB with non-auth mode and create a root admin user.
 
 * [Optional] Execute authentication test for the created user
   ```
@@ -10,19 +10,12 @@
   ```
 This command should return the ``OK`` code
 
+* Down the non-authen instance to start the main docker compose file by run ``docker-compose down``
+
 ### Docker build for DEV environment
-* Navigate to ``marketplace-service`` and execute maven build for spring-boot app to get the war file:
-  ```
-    mvn clean install -DskipTests
-  ```
+* Navigate to ``marketplace-build``
 
-* Navigate to ``marketplace-ui`` and execute node install and run the angular app to get the dist folder:
-  ```
-    npm instal
-    npm run build --prod
-  ```
-
-* Please run ``docker-compose up --build`` from folder ``marketplace-build`` to start a Marketplace DEV at the local
+* Run ``docker-compose up --build`` to start a Marketplace DEV at the local
 
 ### Docker release
 To release a new version for marketplace images, please trigger the ``Docker Release`` actions.
--- marketplace-service/src/main/java/com/axonivy/market/config/MongoConfig.java
+++ marketplace-service/src/main/java/com/axonivy/market/config/MongoConfig.java
@@ -1,9 +1,5 @@
 package com.axonivy.market.config;
 
-import com.mongodb.ConnectionString;
-import com.mongodb.MongoClientSettings;
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoClients;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,13 +13,18 @@ import org.springframework.data.mongodb.core.convert.MongoCustomConversions;
 import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+
 @Configuration
 @EnableMongoRepositories(basePackages = "com.axonivy.market.repository")
 @EnableMongoAuditing
 public class MongoConfig extends AbstractMongoClientConfiguration {
 
-	@Value("${spring.data.mongodb.host}")
-	private String host;
+  @Value("${spring.data.mongodb.uri}")
+  private String uri;
 
 	@Value("${spring.data.mongodb.database}")
 	private String databaseName;
@@ -33,14 +34,14 @@ public class MongoConfig extends AbstractMongoClientConfiguration {
 		return databaseName;
 	}
 
-	@Override
-	public MongoClient mongoClient() {
-		ConnectionString connectionString = new ConnectionString(host);
-		MongoClientSettings mongoClientSettings = MongoClientSettings.builder().applyConnectionString(connectionString)
-				.build();
+  @Override
+  public MongoClient mongoClient() {
+    ConnectionString connectionString = new ConnectionString(uri);
+    MongoClientSettings mongoClientSettings = MongoClientSettings.builder().applyConnectionString(connectionString)
+        .build();
 
-		return MongoClients.create(mongoClientSettings);
-	}
+    return MongoClients.create(mongoClientSettings);
+  }
 
 	/**
 	 * By default, the key in hash map is not allow to contain dot character (.) we
