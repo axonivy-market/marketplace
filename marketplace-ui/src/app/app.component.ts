a218a31c5001bcf532fd9eb705ed6e431661c7f7
Dinh Nguyen <dinh.nguyentrinhquoc@axonactive.com>
2024-07-22 11:32:22 +0700

    update service

--- marketplace-ui/src/app/app.component.ts
+++ marketplace-ui/src/app/app.component.ts
@@ -1,8 +1,14 @@
-import { Component, inject } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject, signal } from '@angular/core';
+import {
+  RouterOutlet,
+  ActivatedRoute,
+  Router,
+  NavigationEnd
+} from '@angular/router';
 import { FooterComponent } from './shared/components/footer/footer.component';
 import { HeaderComponent } from './shared/components/header/header.component';
 import { LoadingService } from './core/services/loading/loading.service';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -13,4 +19,21 @@ import { LoadingService } from './core/services/loading/loading.service';
 })
 export class AppComponent {
   loadingService = inject(LoadingService);
+  isDesignerViewer = signal(false);
+
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe(() => {
+        this.route.queryParams.subscribe(params => {
+          console.log(params['ivy-viewer']);
+          console.log(params['ivy-version']);
+        });
+      });
+  }
 }
