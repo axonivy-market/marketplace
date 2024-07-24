1f10fb554be75267a50b52561783b0fc4404bae1
Hoan Nguyen <hoan.nguyen01@axonactive.com>
2024-07-24 15:09:06 +0700

    Added revert proxy for server to avoid cors check

--- marketplace-ui/src/environments/environment.ts
+++ marketplace-ui/src/environments/environment.ts
@@ -1,8 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://marketplace.server.ivy-cloud.com:8080/marketplace-service',
+  apiUrl: '/marketplace-service',
   githubClientId: 'Ov23liVMliBxBqdQ7FnG',
-  githubAuthCallbackUrl: 'http://marketplace.server.ivy-cloud.com:4200/auth/github/callback',
+  githubAuthCallbackUrl: '/auth/github/callback',
   dayInMiliseconds: 86400000
-
 };
