2f8180682d1d6f9763ce515ff988a912b70010a5
Hoan Nguyen <hoan.nguyen01@axonactive.com>
2024-07-25 14:31:03 +0700

    Introduce new env

---
+++ marketplace-ui/src/environments/environment.staging.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:9090/marketplace-service',
+  githubClientId: 'Ov23li5r26hRBOXZhtLV',
+  githubAuthCallbackPath: '/auth/github/callback',
+  dayInMiliseconds: 86400000
+};
