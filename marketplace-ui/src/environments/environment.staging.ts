9d7c389bda8e62ea22234fc46ae4dda7a46a1fa0
Hoan Nguyen <hoan.nguyen01@axonactive.com>
2024-07-25 15:26:03 +0700

    remove host

--- marketplace-ui/src/environments/environment.staging.ts
+++ marketplace-ui/src/environments/environment.staging.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:9090/marketplace-service',
+  apiUrl: '/marketplace-service',
   githubClientId: 'Ov23li5r26hRBOXZhtLV',
   githubAuthCallbackPath: '/auth/github/callback',
   dayInMiliseconds: 86400000
