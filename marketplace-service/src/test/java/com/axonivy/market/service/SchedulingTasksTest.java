49b0ad64d812c3ab2b3ef72d9356074df04d061b
Pham Hoang Hung <84316773+phhung-axonivy@users.noreply.github.com>
2024-08-15 08:39:38 +0700

    MARP-808 CMS-Editor is missing name and description - fix test (#100)

--- marketplace-service/src/test/java/com/axonivy/market/service/SchedulingTasksTest.java
+++ marketplace-service/src/test/java/com/axonivy/market/service/SchedulingTasksTest.java
@@ -14,7 +14,7 @@ import com.axonivy.market.schedulingtask.ScheduledTasks;
 @SpringBootTest(properties = { "MONGODB_USERNAME=user", "MONGODB_PASSWORD=password", "MONGODB_HOST=mongoHost",
     "MONGODB_DATABASE=product", "MARKET_GITHUB_OAUTH_APP_CLIENT_ID=clientId",
     "MARKET_GITHUB_OAUTH_APP_CLIENT_SECRET=clientSecret", "MARKET_JWT_SECRET_KEY=jwtSecret",
-    "MARKET_CORS_ALLOWED_ORIGIN=*" })
+    "MARKET_CORS_ALLOWED_ORIGIN=*", "MARKET_GITHUB_MARKET_BRANCH=master" })
 class SchedulingTasksTest {
 
   @SpyBean
