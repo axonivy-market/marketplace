3d4cd81bb963034d4376fc0b75062aa9317f080c
PHAM HOANG HUNG <hung.pham@axonactive.com>
2024-08-30 16:36:20 +0700

    MARP-612 Storing number of downloads version - remove unused imports

--- marketplace-service/src/main/java/com/axonivy/market/model/DesignerInstallation.java
+++ marketplace-service/src/main/java/com/axonivy/market/model/DesignerInstallation.java
@@ -1,17 +1,10 @@
 package com.axonivy.market.model;
 
-import com.axonivy.market.entity.MavenArtifactModel;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonInclude;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.apache.commons.lang3.builder.EqualsBuilder;
-import org.apache.commons.lang3.builder.HashCodeBuilder;
-
-import java.util.List;
 
 @Getter
 @Setter
--- marketplace-service/src/main/java/com/axonivy/market/service/ProductDesignerInstallationService.java
+++ marketplace-service/src/main/java/com/axonivy/market/service/ProductDesignerInstallationService.java
@@ -1,12 +1,6 @@
 package com.axonivy.market.service;
 
-import com.axonivy.market.entity.Product;
-import com.axonivy.market.entity.ProductDesignerInstallation;
-import com.axonivy.market.exceptions.model.InvalidParamException;
 import com.axonivy.market.model.DesignerInstallation;
-import com.axonivy.market.model.ProductCustomSortRequest;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
