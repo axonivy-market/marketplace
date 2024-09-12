ed702b368c4cd580b5d9796d54a95bc6ef9fd283
Dinh Nguyen <dinh.nguyentrinhquoc@axonactive.com>
2024-09-12 10:47:23 +0700

    Update ProductServiceImplTest.java

--- marketplace-service/src/test/java/com/axonivy/market/service/impl/ProductServiceImplTest.java
+++ marketplace-service/src/test/java/com/axonivy/market/service/impl/ProductServiceImplTest.java
@@ -12,7 +12,6 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.ArgumentMatchers.anyList;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
