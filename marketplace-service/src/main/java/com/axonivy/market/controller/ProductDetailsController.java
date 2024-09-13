bbc6454807c3df4fda925c5a060175a487d19b66
Dinh Nguyen <dinh.nguyentrinhquoc@axonactive.com>
2024-09-13 17:40:37 +0700

    MARP-1062 update test case fix failed test

--- marketplace-service/src/main/java/com/axonivy/market/controller/ProductDetailsController.java
+++ marketplace-service/src/main/java/com/axonivy/market/controller/ProductDetailsController.java
@@ -21,7 +21,6 @@ import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
