cefb22dbc3139a8dfe30b731bee696e93fea8460
Trần Vĩnh Thiện Phúc <phuc.tranvinhthien@axonactive.com>
2024-09-16 16:32:44 +0700

    MARP-1058 remove unused code

--- marketplace-service/src/main/java/com/axonivy/market/constants/RequestParamConstants.java
+++ marketplace-service/src/main/java/com/axonivy/market/constants/RequestParamConstants.java
@@ -6,7 +6,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class RequestParamConstants {
   public static final String ID = "id";
-  public static final String TAG = "tag";
   public static final String TYPE = "type";
   public static final String KEYWORD = "keyword";
   public static final String LANGUAGE = "language";
@@ -17,5 +16,4 @@ public class RequestParamConstants {
   public static final String SHOW_DEV_VERSION = "isShowDevVersion";
   public static final String DESIGNER_VERSION = "designerVersion";
   public static final String VERSION = "version";
-  public static final String PRODUCT_ID = "productId";
 }
