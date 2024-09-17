7ff7c32e03735989b9f21d74d967c02cd09d1fa1
Trần Vĩnh Thiện Phúc <phuc.tranvinhthien@axonactive.com>
2024-09-17 14:19:54 +0700

    MARP-1058 remove logo.pipe

--- marketplace-ui/src/app/modules/product/product-card/product-card.component.ts
+++ marketplace-ui/src/app/modules/product/product-card/product-card.component.ts
@@ -4,14 +4,13 @@ import { TranslateModule } from '@ngx-translate/core';
 import { LanguageService } from '../../../core/services/language/language.service';
 import { ThemeService } from '../../../core/services/theme/theme.service';
 import { Product } from '../../../shared/models/product.model';
-import { ProductLogoPipe } from '../../../shared/pipes/logo.pipe';
 import { MultilingualismPipe } from '../../../shared/pipes/multilingualism.pipe';
 import { ProductComponent } from '../product.component';
 
 @Component({
   selector: 'app-product-card',
   standalone: true,
-  imports: [CommonModule, ProductLogoPipe, MultilingualismPipe, TranslateModule, NgOptimizedImage],
+  imports: [CommonModule, MultilingualismPipe, TranslateModule, NgOptimizedImage],
   templateUrl: './product-card.component.html',
   styleUrl: './product-card.component.scss'
 })
