bf987456fad6303518b1173612bb96c2c089d6e5
Dinh Nguyen <dinh.nguyentrinhquoc@axonactive.com>
2024-09-18 17:33:56 +0700

    Update MavenArtifactVersion.java

--- marketplace-service/src/main/java/com/axonivy/market/entity/MavenArtifactVersion.java
+++ marketplace-service/src/main/java/com/axonivy/market/entity/MavenArtifactVersion.java
@@ -9,7 +9,6 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serial;
 import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
