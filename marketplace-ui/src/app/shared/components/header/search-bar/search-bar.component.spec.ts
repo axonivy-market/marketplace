9d12bd9f16936833ccf7d95d278528cce9f2b03a
Phạm Duy Linh <138570547+linhpd-axonivy@users.noreply.github.com>
2024-09-23 11:03:07 +0700

    MARP-994: Market-website-Artifact-and-version-in-download-feature-not save-update (#129)

--- marketplace-ui/src/app/shared/components/header/search-bar/search-bar.component.spec.ts
+++ marketplace-ui/src/app/shared/components/header/search-bar/search-bar.component.spec.ts
@@ -3,6 +3,7 @@ import { By } from '@angular/platform-browser';
 import { SearchBarComponent } from './search-bar.component';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
 import { Viewport } from 'karma-viewport/dist/adapter/viewport';
+import { ElementRef } from '@angular/core';
 
 declare const viewport: Viewport;
 
@@ -13,7 +14,9 @@ describe('SearchBarComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [SearchBarComponent, TranslateModule.forRoot()],
-      providers: [TranslateService]
+      providers: [TranslateService,
+        { provide: ElementRef, useValue: { nativeElement: document.createElement('div') } }
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(SearchBarComponent);
@@ -108,5 +111,20 @@ describe('SearchBarComponent', () => {
     expect(smallScreenButton.textContent.trim()).toContain(
       'common.downloadLatestLTSVersion'
     );
+  });
+
+  it('should set isSearchBarDisplayed to false when clicking outside', () => {
+    // Set up the DOM
+    const outsideClickEvent = new MouseEvent('click', {
+      bubbles: true,
+      cancelable: true,
+      view: window
+    });
+
+    // Dispatch a click event to the document
+    document.dispatchEvent(outsideClickEvent);
+
+    // Verify the behavior
+    expect(component.isSearchBarDisplayed()).toBeFalse();
   });
 });
