ea35c589ca1be5ba4f2f43add7f5aa81f89992f7
Pham Hoang Hung <84316773+phhung-axonivy@users.noreply.github.com>
2024-10-22 14:49:04 +0700

    MARP-1079 Title on tab should include connector name (#205)

--- marketplace-ui/src/app/app.routes.ts
+++ marketplace-ui/src/app/app.routes.ts
@@ -2,15 +2,18 @@ import { Routes } from '@angular/router';
 import { GithubCallbackComponent } from './auth/github-callback/github-callback.component';
 import { ErrorPageComponent } from './shared/components/error-page/error-page.component';
 import { RedirectPageComponent } from './shared/components/redirect-page/redirect-page.component';
+import { ERROR_PAGE } from './shared/constants/common.constant';
 
 export const routes: Routes = [
   {
     path: 'error-page',
-    component: ErrorPageComponent
+    component: ErrorPageComponent,
+    title: ERROR_PAGE
   },
   {
     path: 'error-page/:id',
-    component: ErrorPageComponent
+    component: ErrorPageComponent,
+    title: ERROR_PAGE
   },
   {
     path: '',
