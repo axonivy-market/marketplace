ef68652b77aae2cfb97d4dd7fa61ec26bd9889ec
PHAM HOANG HUNG <hung.pham@axonactive.com>
2024-10-25 16:00:04 +0700

    Update tittle on home tab after returning from the market detail

--- marketplace-ui/src/app/modules/home/home.component.ts
+++ marketplace-ui/src/app/modules/home/home.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { ProductComponent } from '../product/product.component';
+import { TranslateService } from '@ngx-translate/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [ProductComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss',
+  styleUrl: './home.component.scss'
 })
-export class HomeComponent {}
+export class HomeComponent {
+  translateService = inject(TranslateService);
+
+  constructor(private readonly titleService: Title) {
+    this.titleService.setTitle(this.translateService.instant('common.branch'));
+  }
+}
