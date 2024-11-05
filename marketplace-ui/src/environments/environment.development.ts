9b2fb1d9a7dfaf4a1eefa76834d03150a4e29d40
Hoang Vu Huy <vhhoang@axonactive.com>
2024-11-05 10:55:12 +0700

    Set up domain name for Matomo

--- marketplace-ui/src/environments/environment.development.ts
+++ marketplace-ui/src/environments/environment.development.ts
@@ -1,10 +1,9 @@
 export const environment = {
   production: false,
-  // apiUrl: 'http://localhost:9090/marketplace-service',
-  apiUrl: 'http://localhost:8080',
+  apiUrl: 'http://localhost:9090/marketplace-service',
   githubClientId: 'Iv23livu9HbsC4Q24eSC',
   githubAuthCallbackPath: '/auth/github/callback',
   dayInMiliseconds: 86400000,
   matomoSiteId: 1,
-  matomoTrackerUrl: '//localhost:8085/'
+  matomoTrackerUrl: '//marketplace.server.ivy-cloud.com::8085/'
 };
