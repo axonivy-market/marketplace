7856a457770f20f229c0a3b890d14622f7d6c97c
Khanh Nguyen <khanh.nguyenduy@axonactive.com>
2024-11-14 14:38:08 +0700

    Handle feedbacks

--- marketplace-ui/src/app/shared/pipes/character-count.pipe.ts
+++ marketplace-ui/src/app/shared/pipes/character-count.pipe.ts
@@ -1,6 +1,5 @@
 import { inject, Pipe, PipeTransform } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
-import { Feedback } from '../models/feedback.model';
 import { MAX_FEEDBACK_LENGTH } from '../constants/common.constant';
 
 @Pipe({
@@ -9,9 +8,7 @@ import { MAX_FEEDBACK_LENGTH } from '../constants/common.constant';
 })
 export class CharacterCountPipe implements PipeTransform {
   translateService = inject(TranslateService);
-  transform(feedback: Feedback): string {
-    console.log('ndkhanh');
-    
-    return `${feedback.content.length}/${MAX_FEEDBACK_LENGTH}`;
+  transform(value: string): string {   
+    return `${value.length}/${MAX_FEEDBACK_LENGTH}`;
   }
 }
