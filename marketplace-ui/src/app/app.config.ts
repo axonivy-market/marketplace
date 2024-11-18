862d615c06ec49032e90332446441a7e23df2b09
Hoang Vu Huy <vhhoang@axonactive.com>
2024-11-18 13:57:10 +0700

    Remove unused import

--- marketplace-ui/src/app/app.config.ts
+++ marketplace-ui/src/app/app.config.ts
@@ -7,8 +7,6 @@ import { MARKED_OPTIONS, MarkdownModule } from 'ngx-markdown';
 import { markedOptionsFactory } from './core/configs/markdown.config';
 import { httpLoaderFactory } from './core/configs/translate.config';
 import { apiInterceptor } from './core/interceptors/api.interceptor';
-import { provideMatomo, withRouter } from 'ngx-matomo-client';
-import { environment } from '../environments/environment';
 
 const scrollConfig: InMemoryScrollingOptions = {
   scrollPositionRestoration: 'disabled',
