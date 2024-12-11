37e57b5b8fa11f0d7cc87150e1d745eec4d7f3c0
Dinh Nguyen <dinh.nguyentrinhquoc@axonactive.com>
2024-12-11 19:11:14 +0700

    update service

--- marketplace-ui/src/app/core/services/loading/loading.service.ts
+++ marketplace-ui/src/app/core/services/loading/loading.service.ts
@@ -1,35 +1,24 @@
-import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { Injectable, signal } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoadingService {
-  private readonly loadingSubject = new BehaviorSubject<{
-    [key: string]: boolean;
-  }>({});
-  loading$ = this.loadingSubject.asObservable;
+  loadingStates = signal<{ [key: string]: boolean }>({});
 
-  private setLoading(componentIds: string[], isLoading: boolean): void {
-    const currentState = this.loadingSubject.value;
-
-    // Update the loading state for each component ID
-    componentIds.forEach(id => {
-      currentState[id] = isLoading;
+  private setLoading(componentId: string, isLoading: boolean): void {
+    this.loadingStates.update(states => {
+      const updatedStates = { ...states };
+      updatedStates[componentId] = isLoading;
+      return updatedStates;
     });
-
-    this.loadingSubject.next({ ...currentState });
   }
 
-  showLoading(...componentIds: string[]): void {
-    this.setLoading(componentIds, true);
+  showLoading(componentId: string): void {
+    this.setLoading(componentId, true);
   }
 
-  hideLoading(...componentIds: string[]) {
-    this.setLoading(componentIds, false);
-  }
-
-  isLoading(compnentId: string): boolean {
-    return this.loadingSubject.value[compnentId] || false;
+  hideLoading(componentId: string) {
+    this.setLoading(componentId, false);
   }
 }
