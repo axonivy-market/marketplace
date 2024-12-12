aabb352656103d5e50d16f8273b77cec87d1b356
nntthuy-axonivy <thuy.nguyenngocthanh@axonactive.com>
2024-12-12 15:08:34 +0700

    Fix regex get exact h2 name

--- marketplace-service/src/main/java/com/axonivy/market/util/ProductContentUtils.java
+++ marketplace-service/src/main/java/com/axonivy/market/util/ProductContentUtils.java
@@ -22,7 +22,7 @@ import java.util.regex.Pattern;
 import static com.axonivy.market.constants.ProductJsonConstants.DEFAULT_PRODUCT_TYPE;
 
 public class ProductContentUtils {
-  public static final String DEMO_SETUP_TITLE = "(?i)## Demo|## Setup";
+  public static final String DEMO_SETUP_TITLE = "(?m)^## (Demo|Setup)$";
   private static final String HASH = "#";
   public static final String DESCRIPTION = "description";
   public static final String DEMO = "demo";
