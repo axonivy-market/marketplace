d2ca28e529fd52b5ec5aaa48074ddde7db5c71a7
PHAM HOANG HUNG <hung.pham@axonactive.com>
2024-12-13 10:30:35 +0700

    Remove redundant import

--- marketplace-service/src/test/java/com/axonivy/market/controller/ProductControllerTest.java
+++ marketplace-service/src/test/java/com/axonivy/market/controller/ProductControllerTest.java
@@ -37,7 +37,6 @@ import java.util.Map;
 import java.util.Objects;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
