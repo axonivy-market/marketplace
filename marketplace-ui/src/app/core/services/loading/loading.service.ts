d5985a97ef663ab0a23a53f89eac8c3485f3b226
Dinh Nguyen <dinh.nguyentrinhquoc@axonactive.com>
2024-12-17 14:49:25 +0700

    update code

--- marketplace-ui/src/app/core/services/loading/loading.service.ts
+++ marketplace-ui/src/app/core/services/loading/loading.service.ts
@@ -5,7 +5,6 @@ import { Injectable, signal } from '@angular/core';
 })
 export class LoadingService {
   loadingStates = signal<{ [key: string]: boolean }>({});
-  activeCallCount = signal<number>(0);
 
   private setLoading(componentId: string, isLoading: boolean): void {
     this.loadingStates.update(states => {
@@ -16,14 +15,10 @@ export class LoadingService {
   }
 
   showLoading(componentId: string): void {
-    this.activeCallCount.set(this.activeCallCount() + 1);
     this.setLoading(componentId, true);
   }
 
   hideLoading(componentId: string) {
-    if (this.activeCallCount() > 0) {
-      this.activeCallCount.set(this.activeCallCount() - 1);
-    }
     this.setLoading(componentId, false);
   }
 }
