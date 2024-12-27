5dbe2cad54d7179896f7b73d0244467c50f77184
nntthuy-axonivy <thuy.nguyenngocthanh@axonactive.com>
2024-12-27 17:32:44 +0700

    Refactor files

--- marketplace-service/src/main/java/com/axonivy/market/config/LimitCallingConfig.java
+++ marketplace-service/src/main/java/com/axonivy/market/config/LimitCallingConfig.java
@@ -2,25 +2,30 @@ package com.axonivy.market.config;
 
 import io.github.bucket4j.Bandwidth;
 import io.github.bucket4j.Bucket;
-import io.github.bucket4j.Refill;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 import java.time.Duration;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
+@Log4j2
 @Component
-public class RateLimitFilter extends OncePerRequestFilter {
-  private static final String REQUEST_PATH = "/api";
-  @Value("${market.allowed.download-capacity}")
+public class LimitCallingConfig extends OncePerRequestFilter {
+  private static final String REQUEST_HEADER = "X-Forwarded-For";
+  @Value("${market.allowed.click-capacity}")
   private int capacity;
+
+  @Value("${market.limited.request-paths}")
+  private List<String> requestPaths;
   private final Map<String, Bucket> buckets = new ConcurrentHashMap<>();
 
   @Override
@@ -29,15 +34,13 @@ public class RateLimitFilter extends OncePerRequestFilter {
 
     String clientIp = getClientIp(request);
     String apiPath = request.getRequestURI();
-
-    if (apiPath.contains(REQUEST_PATH)) {
+    if (requestPaths.stream().anyMatch(path -> apiPath.matches(path + ".*"))) {
       Bucket bucket = buckets.computeIfAbsent(clientIp, this::createNewBucket);
 
       if (bucket.tryConsume(1)) {
-        System.out.println("Request allowed for IP: " + clientIp + ". Remaining tokens: " + bucket.getAvailableTokens());
+        log.warn("Request allowed for IP: {}. Remaining tokens: {}", clientIp, bucket.getAvailableTokens());
         filterChain.doFilter(request, response);
       } else {
-        System.out.println("Too many requests from IP: " + clientIp);
         response.setStatus(HttpServletResponse.SC_BAD_GATEWAY);
         response.getWriter().write("Too many requests. Please try again later.");
       }
@@ -47,12 +50,15 @@ public class RateLimitFilter extends OncePerRequestFilter {
   }
 
   private Bucket createNewBucket(String clientIp) {
-    Bandwidth limit = Bandwidth.classic(capacity, Refill.greedy(10, Duration.ofMinutes(2)));
+    Bandwidth limit = Bandwidth.builder()
+        .capacity(capacity)
+        .refillGreedy(capacity, Duration.ofMinutes(1))
+        .build();
     return Bucket.builder().addLimit(limit).build();
   }
 
   private String getClientIp(HttpServletRequest request) {
-    String forwardedFor = request.getHeader("X-Forwarded-For");
+    String forwardedFor = request.getHeader(REQUEST_HEADER);
     if (forwardedFor != null && !forwardedFor.isEmpty()) {
       return forwardedFor.split(",")[0];
     }
