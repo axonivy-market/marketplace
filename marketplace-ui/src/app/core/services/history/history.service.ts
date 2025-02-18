62444e7ba5a833d389088acb1b53629edd9bfd5c
Dinh Nguyen <dinh.nguyentrinhquoc@axonactive.com>
2025-02-18 11:38:15 +0700

    Update history.service.ts

--- marketplace-ui/src/app/core/services/history/history.service.ts
+++ marketplace-ui/src/app/core/services/history/history.service.ts
@@ -3,15 +3,18 @@ import { SortOption } from '../../../shared/enums/sort-option.enum';
 import { FILTER_TYPES } from '../../../shared/constants/common.constant';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 export class HistoryService {
-    lastSearchText: WritableSignal<String> = signal("");
-    lastSortOption: WritableSignal<SortOption> = signal(SortOption.STANDARD);
-    lastSearchType: WritableSignal<String> = signal(FILTER_TYPES[0].value);
+  lastSearchText: WritableSignal<string> = signal('');
+  lastSortOption: WritableSignal<SortOption> = signal(SortOption.STANDARD);
+  lastSearchType: WritableSignal<string> = signal(FILTER_TYPES[0].value);
 
-    isLastSearchChanged():boolean {
-        return this.lastSearchText() !== "" || this.lastSortOption() !== SortOption.STANDARD
-            || this.lastSearchType() !== FILTER_TYPES[0].value;
-    }
+  isLastSearchChanged(): boolean {
+    return (
+      this.lastSearchText() !== '' ||
+      this.lastSortOption() !== SortOption.STANDARD ||
+      this.lastSearchType() !== FILTER_TYPES[0].value
+    );
+  }
 }
