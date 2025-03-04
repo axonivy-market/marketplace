8e932970429e5140fbb4e526ebad1ed01470b601
nqhoan-axonivy <hoan.nguyen01@axonactive.com>
2025-03-04 16:28:06 +0700

    MARP-2019 Switch to a new DB for marketplace server
    Fix SchedulingTasksTest  cannot init jpa driver

--- marketplace-service/src/test/java/com/axonivy/market/service/impl/SchedulingTasksTest.java
+++ marketplace-service/src/test/java/com/axonivy/market/service/impl/SchedulingTasksTest.java
@@ -1,39 +1,48 @@
-//package com.axonivy.market.service.impl;
-//
-//import com.axonivy.market.schedulingtask.ScheduledTasks;
-//import org.awaitility.Awaitility;
-//import org.awaitility.Durations;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.boot.test.mock.mockito.SpyBean;
-//
-//import static org.mockito.Mockito.atLeast;
-//import static org.mockito.Mockito.verify;
-//
-//@SpringBootTest(properties = {"MONGODB_USERNAME=user", "MONGODB_PASSWORD=password", "MONGODB_HOST=mongoHost",
-//    "MONGODB_DATABASE=product", "MARKET_GITHUB_OAUTH_APP_CLIENT_ID=clientId",
-//    "MARKET_GITHUB_OAUTH_APP_CLIENT_SECRET=clientSecret", "MARKET_JWT_SECRET_KEY=jwtSecret",
-//    "MARKET_CORS_ALLOWED_ORIGIN=*", "MARKET_GITHUB_MARKET_BRANCH=master", "MARKET_MONGO_LOG_LEVEL=DEBUG",
-//    "MARKET_LOG_PATH=logs", "MARKET_GITHUB_TOKEN=test", "MARKET_CLICK_LIMIT=5",
-//    "MARKET_LIMITED_REQUEST_PATHS=/api/test"})
-//class SchedulingTasksTest {
-//
-//  @SpyBean
-//  ScheduledTasks tasks;
-//
-//  @Test
-//  void testShouldNotTriggerAfterApplicationStarted() {
-//    Awaitility.await().atMost(Durations.TEN_SECONDS)
-//        .untilAsserted(() -> verify(tasks, atLeast(0)).syncDataForProductFromGitHubRepo());
-//
-//    Awaitility.await().atMost(Durations.TEN_SECONDS)
-//        .untilAsserted(() -> verify(tasks, atLeast(0)).syncDataForProductDocuments());
-//
-//    Awaitility.await().atMost(Durations.TEN_SECONDS)
-//        .untilAsserted(() -> verify(tasks, atLeast(0)).syncDataForProductMavenDependencies());
-//
-//    Awaitility.await().atMost(Durations.TEN_SECONDS)
-//        .untilAsserted(() -> verify(tasks, atLeast(0)).syncDataForProductReleases());
-//  }
-//
-//}
+package com.axonivy.market.service.impl;
+
+import com.axonivy.market.schedulingtask.ScheduledTasks;
+import org.awaitility.Awaitility;
+import org.awaitility.Durations;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+
+import static org.mockito.Mockito.atLeast;
+import static org.mockito.Mockito.verify;
+
+@SpringBootTest(properties = {"MARKET_GITHUB_OAUTH_APP_CLIENT_ID=clientId",
+    "MARKET_GITHUB_OAUTH_APP_CLIENT_SECRET=clientSecret",
+    "MARKET_JWT_SECRET_KEY=jwtSecret",
+    "MARKET_CORS_ALLOWED_ORIGIN=*",
+    "MARKET_GITHUB_MARKET_BRANCH=master",
+    "MARKET_MONGO_LOG_LEVEL=DEBUG",
+    "MARKET_LOG_PATH=logs",
+    "MARKET_GITHUB_TOKEN=test",
+    "MARKET_CLICK_LIMIT=5",
+    "MARKET_LIMITED_REQUEST_PATHS=/api/test",
+    "spring.datasource.url=jdbc:h2:mem:testdb",
+    "spring.datasource.username=sa",
+    "spring.datasource.password=password",
+    "spring.datasource.driver-class-name=org.h2.Driver"},
+    webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+class SchedulingTasksTest {
+
+  @SpyBean
+  ScheduledTasks tasks;
+
+  @Test
+  void testShouldNotTriggerAfterApplicationStarted() {
+    Awaitility.await().atMost(Durations.TEN_SECONDS)
+        .untilAsserted(() -> verify(tasks, atLeast(0)).syncDataForProductFromGitHubRepo());
+
+    Awaitility.await().atMost(Durations.TEN_SECONDS)
+        .untilAsserted(() -> verify(tasks, atLeast(0)).syncDataForProductDocuments());
+
+    Awaitility.await().atMost(Durations.TEN_SECONDS)
+        .untilAsserted(() -> verify(tasks, atLeast(0)).syncDataForProductMavenDependencies());
+
+    Awaitility.await().atMost(Durations.TEN_SECONDS)
+        .untilAsserted(() -> verify(tasks, atLeast(0)).syncDataForProductReleases());
+  }
+
+}
