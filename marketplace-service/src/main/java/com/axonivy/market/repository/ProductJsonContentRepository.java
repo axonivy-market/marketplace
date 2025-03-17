ff11bb8cda247775aae7d9761a70b21e75b6e6c3
Trần Vĩnh Thiện Phúc <143604440+tvtphuc-axonivy@users.noreply.github.com>
2025-03-17 17:36:55 +0700

    MARP-2202 Add transactional for deleteAllByProductId (#325)

--- marketplace-service/src/main/java/com/axonivy/market/repository/ProductJsonContentRepository.java
+++ marketplace-service/src/main/java/com/axonivy/market/repository/ProductJsonContentRepository.java
@@ -1,7 +1,11 @@
 package com.axonivy.market.repository;
 
 import com.axonivy.market.entity.ProductJsonContent;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,5 +17,8 @@ public interface ProductJsonContentRepository extends JpaRepository<ProductJsonC
 
   List<ProductJsonContent> findByProductIdAndVersionIn(String productId, List<String> versions);
 
-  void deleteAllByProductId(String productId);
+  @Modifying
+  @Transactional
+  @Query("DELETE FROM ProductJsonContent p WHERE p.productId = :productId")
+  void deleteAllByProductId(@Param("productId") String productId);
 }
