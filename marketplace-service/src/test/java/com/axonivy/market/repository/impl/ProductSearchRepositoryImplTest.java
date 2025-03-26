f05e95218811188a44f0e9f2adf6b7a254be95c2
quanpham-axonivy <quan.phamvan@axonactive.com>
2025-03-26 17:22:58 +0700

    MARP-2205 Filter/Sort/Search issue - Update test (#333)

--- marketplace-service/src/test/java/com/axonivy/market/repository/impl/ProductSearchRepositoryImplTest.java
+++ marketplace-service/src/test/java/com/axonivy/market/repository/impl/ProductSearchRepositoryImplTest.java
@@ -6,6 +6,7 @@ import com.axonivy.market.entity.Product;
 import com.axonivy.market.enums.DocumentField;
 import com.axonivy.market.enums.Language;
 import com.axonivy.market.repository.ProductCustomSortRepository;
+import com.axonivy.market.repository.ProductModuleContentRepository;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.TypedQuery;
 import jakarta.persistence.criteria.*;
@@ -36,6 +37,9 @@ class ProductSearchRepositoryImplTest extends BaseSetup {
   ProductCustomSortRepository productCustomSortRepo;
 
   @Mock
+  ProductModuleContentRepository contentRepository;
+
+  @Mock
   private EntityManager em;
 
   @InjectMocks
@@ -51,9 +55,12 @@ class ProductSearchRepositoryImplTest extends BaseSetup {
   @Test
   void testSearchByCriteria() {
     TypedQuery<Product> query = mock(TypedQuery.class);
+    TypedQuery<Long> countQuery = mock(TypedQuery.class);
     CriteriaBuilder cb = mock(CriteriaBuilder.class);
     CriteriaQuery<Product> criteriaQuery = mock(CriteriaQuery.class);
+    CriteriaQuery<Long> criteriaCountQuery = mock(CriteriaQuery.class);
     Root<Product> productRoot = mock(Root.class);
+    Root<Product> countRoot = mock(Root.class);
 
     Path<String> nameValue = mock(Path.class);
     Path<String> keyValue = mock(Path.class);
@@ -66,6 +73,7 @@ class ProductSearchRepositoryImplTest extends BaseSetup {
     Predicate predicate = mock(Predicate.class);
     when(productListedRepository.buildCriteriaSearch(searchCriteria, cb, productRoot)).thenReturn(predicate);
 
+    // For query products
     when(em.getCriteriaBuilder()).thenReturn(cb);
     when(cb.createQuery(Product.class)).thenReturn(criteriaQuery);
     when(criteriaQuery.from(Product.class)).thenReturn(productRoot);
@@ -82,13 +90,77 @@ class ProductSearchRepositoryImplTest extends BaseSetup {
     when(caseExpression.when(any(), any())).thenReturn(caseExpression);
     when(caseExpression.otherwise(any())).thenReturn(nameValue); // Should return a valid expression
 
-    Page<Product> result = productListedRepository.searchByCriteria(searchCriteria, PAGEABLE);
+    // For counting
+    when(cb.createQuery(Long.class)).thenReturn(criteriaCountQuery);
+    when(criteriaCountQuery.from(Product.class)).thenReturn(countRoot);
+    when(productListedRepository.buildCriteriaSearch(searchCriteria, cb, countRoot)).thenReturn(predicate);
+    when(criteriaCountQuery.select(any())).thenReturn(criteriaCountQuery);
+    when(criteriaCountQuery.where(predicate)).thenReturn(criteriaCountQuery);
+    when(em.createQuery(criteriaCountQuery)).thenReturn(countQuery);
+    when(countQuery.getSingleResult()).thenReturn(4L);
+
+    Page<Product> result = productListedRepository.searchByCriteria(searchCriteria, PAGEABLE_ALPHABETICALLY);
 
     assertFalse(result.isEmpty(), "Result is empty");
     assertTrue(result.isFirst(), "Result is not on the first page");
     assertEquals(2, result.getContent().size(), "Unexpected number of products");
     assertTrue(result.getContent().get(0).getNames().containsValue(SAMPLE_PRODUCT_NAME),
         "Expected product name not found in the result");
+
+  }
+
+  @Test
+  void testSearchByCriteriaOrderByStandard() {
+    TypedQuery<Product> query = mock(TypedQuery.class);
+    CriteriaBuilder cb = mock(CriteriaBuilder.class);
+    CriteriaQuery<Product> criteriaQuery = mock(CriteriaQuery.class);
+    Root<Product> productRoot = mock(Root.class);
+
+    Path<String> nameValue = mock(Path.class);
+    Path<String> keyValue = mock(Path.class);
+
+    MapJoin<Product, String, String> namesJoin = mock(MapJoin.class);
+    Mockito.<MapJoin<Product, String, String>>when(productRoot.joinMap(any(),any())).thenReturn(namesJoin);
+    when(namesJoin.value()).thenReturn(nameValue);
+    when(namesJoin.key()).thenReturn(keyValue);
+
+    Predicate predicate = mock(Predicate.class);
+    when(productListedRepository.buildCriteriaSearch(searchCriteria, cb, productRoot)).thenReturn(predicate);
+
+
+    // For Sort standard
+    var mockPath = mock(Path.class);
+    var mockOrder = mock(Order.class);
+    var mockCoalesce = mock(Expression.class);
+
+    when(productRoot.get(PRODUCT_MARKETPLACE_DATA)).thenReturn(mockPath);
+    when(mockPath.get(CUSTOM_ORDER)).thenReturn(mockPath);
+
+    // Mock coalesce expression
+    when(cb.coalesce(mockPath, Integer.MIN_VALUE)).thenReturn(mockCoalesce);
+
+    // Mock descending order with coalesce
+    when(cb.desc(mockCoalesce)).thenReturn(mockOrder);
+
+    // For query products
+    when(em.getCriteriaBuilder()).thenReturn(cb);
+    when(cb.createQuery(Product.class)).thenReturn(criteriaQuery);
+    when(criteriaQuery.from(Product.class)).thenReturn(productRoot);
+    when(criteriaQuery.select(productRoot)).thenReturn(criteriaQuery);
+    when(criteriaQuery.where(predicate)).thenReturn(criteriaQuery);
+    when(criteriaQuery.orderBy(anyList())).thenReturn(criteriaQuery);
+    when(criteriaQuery.groupBy(productRoot, keyValue, nameValue)).thenReturn(criteriaQuery);
+    when(em.createQuery(criteriaQuery)).thenReturn(query);
+    when(query.getResultList()).thenReturn(mockResultReturn.getContent()); // Mocking a result
+
+    Page<Product> result = productListedRepository.searchByCriteria(searchCriteria, PAGEABLE_STANDARD);
+
+    assertFalse(result.isEmpty(), "Result is empty");
+    assertTrue(result.isFirst(), "Result is not on the first page");
+    assertEquals(2, result.getContent().size(), "Unexpected number of products");
+    assertTrue(result.getContent().get(0).getNames().containsValue(SAMPLE_PRODUCT_NAME),
+            "Expected product name not found in the result");
+
   }
 
   @Test
@@ -165,5 +237,26 @@ class ProductSearchRepositoryImplTest extends BaseSetup {
         "Product MarketDirectory " + result.getMarketDirectory());
   }
 
+  @Test
+  void testGetProductByIdAndVersion() {
+    Product mockProduct = mockResultReturn.getContent().get(0);
+    TypedQuery<Product> query = mock(TypedQuery.class);
+    CriteriaBuilder mockCriteriaBuilder = mock(CriteriaBuilder.class);
+    CriteriaQuery<Product> criteriaQuery = mock(CriteriaQuery.class);
+    Root<Product> productRoot = mock(Root.class);
+
+    when(em.getCriteriaBuilder()).thenReturn(mockCriteriaBuilder);
+    when(mockCriteriaBuilder.createQuery(Product.class)).thenReturn(criteriaQuery);
+    when(criteriaQuery.from(Product.class)).thenReturn(productRoot);
+
+    when(em.createQuery(criteriaQuery)).thenReturn(query);
+    when(query.getSingleResult()).thenReturn(mockProduct);
+
+
+    Product result = productListedRepository.getProductByIdAndVersion(mockProduct.getId(), mockProduct.getVersion());
+
+    assertNotNull(result, "Result is empty");
+    assertEquals(mockProduct.getId(), result.getId(), "Product ID " + result.getId());
+  }
 
 }
