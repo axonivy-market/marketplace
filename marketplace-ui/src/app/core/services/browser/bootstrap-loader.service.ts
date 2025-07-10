63ea1fed18daedfd3d4b0bb1a2704f81e210781b
vhhoang-axonivy <vhhoang@axonactive.com>
2025-07-10 11:50:29 +0700

    Clean up

--- marketplace-ui/src/app/core/services/browser/bootstrap-loader.service.ts
+++ marketplace-ui/src/app/core/services/browser/bootstrap-loader.service.ts
@@ -1,4 +1,3 @@
-// src/app/shared/services/bootstrap-loader.service.ts
 import { Injectable, inject, PLATFORM_ID } from '@angular/core';
 import { isPlatformBrowser } from '@angular/common';
 
@@ -8,9 +7,7 @@ export class BootstrapLoaderService {
 
   async init(): Promise<void> {
     if (isPlatformBrowser(this.platformId)) {
-      // Import Bootstrap JS or other DOM-dependent logic
-      await import('@ng-bootstrap/ng-bootstrap'); // or jQuery plugins
-      // (Optional) also call enableDismissTrigger() if needed
+      await import('@ng-bootstrap/ng-bootstrap');
     }
   }
 }
