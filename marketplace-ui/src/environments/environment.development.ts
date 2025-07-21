c2dcbe92d162c25c37949c0ac90ab603b8d819c0
vhhoang-axonivy <vhhoang@axonactive.com>
2025-07-21 11:36:03 +0700

    Clean up

--- marketplace-ui/src/environments/environment.development.ts
+++ marketplace-ui/src/environments/environment.development.ts
@@ -1,13 +1,11 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080',
+  // apiUrl: 'http://localhost:8080',
+  apiUrl: 'https://market.axonivy.com/marketplace-service',
   githubClientId: 'Iv23livu9HbsC4Q24eSC',
   githubAuthCallbackPath: '/auth/github/callback',
   dayInMiliseconds: 86400000,
   matomoSiteId: 3,
   matomoTrackerUrl: '//market.axonivy.com/matomo-web',
-  githubApiUrl: 'https://api.github.com',
-  googleProgrammableSearchScriptId: 'googleCSEScript',
-  googleProgrammableSearchScriptType: 'text/javascript',
-  googleProgrammableSearchScriptSource: 'https://cse.google.com/cse.js?cx=1434dfc0811d84f59',
+  githubApiUrl: 'https://api.github.com'
 };
