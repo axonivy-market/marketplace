61b2f58263a8cc7bab2422747c28be6ec0574bac
quanpham-axonivy <quan.phamvan@axonactive.com>
2025-08-04 18:15:10 +0700

    MARP-2717 Css Flickering on refresh

--- marketplace-ui/src/app/shared/components/header/theme-selection/theme-selection.component.ts
+++ marketplace-ui/src/app/shared/components/header/theme-selection/theme-selection.component.ts
@@ -1,10 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { ThemeService } from '../../../../core/services/theme/theme.service';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-theme-selection',
   standalone: true,
-  imports: [],
+  imports: [NgIf],
   templateUrl: './theme-selection.component.html',
   styleUrl: './theme-selection.component.scss'
 })
