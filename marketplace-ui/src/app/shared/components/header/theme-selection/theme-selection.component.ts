e158cddb3aad527366a448db8336f75a148c9758
quanpham-axonivy <quan.phamvan@axonactive.com>
2025-08-05 15:40:20 +0700

    fix icon flickering

--- marketplace-ui/src/app/shared/components/header/theme-selection/theme-selection.component.ts
+++ marketplace-ui/src/app/shared/components/header/theme-selection/theme-selection.component.ts
@@ -1,14 +1,31 @@
-import { Component, inject } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { ThemeService } from '../../../../core/services/theme/theme.service';
-import { NgClass } from '@angular/common';
+import { NgClass, NgIf, DOCUMENT } from '@angular/common';
+
+const THEME_ICON_CLASS = 'theme-icon-class';
+const LIGHT_ICON_CLASS = 'bi-moon';
+const DARK_ICON_CLASS = 'bi-sun';
 
 @Component({
   selector: 'app-theme-selection',
   standalone: true,
-  imports: [NgClass],
+  imports: [NgClass, NgIf],
   templateUrl: './theme-selection.component.html',
   styleUrl: './theme-selection.component.scss'
 })
 export class ThemeSelectionComponent {
-  themeService = inject(ThemeService);
+  iconClass: string | null = null;
+
+  constructor(
+    @Inject(DOCUMENT) private readonly document: Document,
+    public themeService: ThemeService
+  ) {
+    this.iconClass =
+      this.document.documentElement.getAttribute(THEME_ICON_CLASS);
+  }
+
+  onToggleTheme(): void {
+    this.themeService.changeTheme();
+    this.iconClass = this.themeService.isDarkMode() ? DARK_ICON_CLASS : LIGHT_ICON_CLASS;
+  }
 }
