8c5b28caed8f14ec75b4fa6d9b28c20616550c6c
Dinh Nguyen <dinh.nguyentrinhquoc@axonactive.com>
2025-08-12 11:57:33 +0700

    Update loading-component-id.ts

--- marketplace-ui/src/app/shared/enums/loading-component-id.ts
+++ marketplace-ui/src/app/shared/enums/loading-component-id.ts
@@ -1,7 +1,6 @@
 export enum LoadingComponentId {
   LANDING_PAGE = 'landing-page',
   END_LANDING_PAGE = 'end-landing-page',
-  PRODUCT_DETAIL_INFORMATION = 'product-detail-information',
   PRODUCT_VERSION = 'product-version',
   DETAIL_PAGE = 'detail-page',
   SECURITY_MONITOR = 'security-monitor',
