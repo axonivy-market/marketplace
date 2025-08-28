2b7120ac216ecd40afce1eb48a5c6717563eb492
vhhoang-axonivy <vhhoang@axonactive.com>
2025-08-28 09:55:34 +0700

    Fix Sonar

--- marketplace-service/src/main/java/com/axonivy/market/comparator/LatestVersionComparator.java
+++ marketplace-service/src/main/java/com/axonivy/market/comparator/LatestVersionComparator.java
@@ -1,8 +1,9 @@
 package com.axonivy.market.comparator;
 
+import java.io.Serializable;
 import java.util.Comparator;
 
-public class LatestVersionComparator implements Comparator<String> {
+public class LatestVersionComparator implements Comparator<String>, Serializable {
 
   @Override
   public int compare(String v1, String v2) {
