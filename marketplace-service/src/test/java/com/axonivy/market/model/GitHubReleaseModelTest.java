2eb91ecbba2d56a24c592aaee7462c4b6cf52778
vhhoang-axonivy <vhhoang@axonactive.com>
2025-09-11 16:44:06 +0700

    Add unit tests for GitHubReleaseModel

---
+++ marketplace-service/src/test/java/com/axonivy/market/model/GitHubReleaseModelTest.java
@@ -0,0 +1,44 @@
+package com.axonivy.market.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+public class GitHubReleaseModelTest {
+
+  @Test
+  void testEqualsAndHashCodeSameName() {
+    GitHubReleaseModel release1 = new GitHubReleaseModel();
+    release1.setName("12.0.3");
+
+    GitHubReleaseModel release2 = new GitHubReleaseModel();
+    release2.setName("12.0.3");
+
+    assertEquals(release1, release2, "Objects with the same name should be equal");
+    assertEquals(release1.hashCode(), release2.hashCode(),
+        "Objects with the same name should have the same hashCode");
+  }
+
+  @Test
+  void testEqualsAndHashCodeDifferentName() {
+    GitHubReleaseModel release1 = new GitHubReleaseModel();
+    release1.setName("12.0.3");
+
+    GitHubReleaseModel release2 = new GitHubReleaseModel();
+    release2.setName("12.0.4");
+
+    assertNotEquals(release1, release2, "Objects with different names should not be equal");
+    assertNotEquals(release1.hashCode(), release2.hashCode(),
+        "Objects with different names should not have the same hashCode");
+  }
+
+  @Test
+  void testEqualsWithNullAndDifferentClass() {
+    GitHubReleaseModel release = new GitHubReleaseModel();
+    release.setName("12.0.3");
+
+    assertNotEquals(release, null, "GitHubReleaseModel should not equal null");
+    assertNotEquals(release, "some string", "GitHubReleaseModel should not equal an object of a different class");
+  }
+}
