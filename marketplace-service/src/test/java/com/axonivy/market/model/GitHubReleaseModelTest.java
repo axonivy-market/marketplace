271e679b08ae1fd373075d077a070e1a7a6105cc
vhhoang-axonivy <vhhoang@axonactive.com>
2025-09-12 16:46:02 +0700

    Clean up

--- marketplace-service/src/test/java/com/axonivy/market/model/GitHubReleaseModelTest.java
+++ marketplace-service/src/test/java/com/axonivy/market/model/GitHubReleaseModelTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class GitHubReleaseModelTest {
 
