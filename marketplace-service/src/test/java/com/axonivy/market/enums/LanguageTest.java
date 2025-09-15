c33a03eb007805e42d0bbcd058641847963f5989
vhhoang-axonivy <vhhoang@axonactive.com>
2025-09-15 11:02:57 +0700

    Clean up

--- marketplace-service/src/test/java/com/axonivy/market/enums/LanguageTest.java
+++ marketplace-service/src/test/java/com/axonivy/market/enums/LanguageTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
-public class LanguageTest {
+class LanguageTest {
   @Test
   void testOfReturnsEnForLowercase() {
     assertEquals(Language.EN, Language.of("en"),
