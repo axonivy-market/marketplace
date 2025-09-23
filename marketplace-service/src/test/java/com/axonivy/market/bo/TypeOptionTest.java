64f8c5acbece5b5eb24bd5a8f4276833d4ffe1c0
vhhoang-axonivy <vhhoang@axonactive.com>
2025-09-23 13:56:44 +0700

    Clean up

--- marketplace-service/src/test/java/com/axonivy/market/bo/TypeOptionTest.java
+++ marketplace-service/src/test/java/com/axonivy/market/bo/TypeOptionTest.java
@@ -8,7 +8,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class TypeOptionTest {
+class TypeOptionTest {
   @Test
   void testShouldReturnConnectorsForValidInput() {
     String input = "connectors";
