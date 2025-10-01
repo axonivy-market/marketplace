59a50b1a46f12df430a10e82ab47fc14e9b4d16d
Thuy Nguyen <145430420+nntthuy-axonivy@users.noreply.github.com>
2025-10-01 15:17:18 +0700

    MARP-2971 Monitoring Pages in the marketplace is not synced by day (#451)

--- marketplace-service/src/main/java/com/axonivy/market/entity/GithubRepo.java
+++ marketplace-service/src/main/java/com/axonivy/market/entity/GithubRepo.java
@@ -15,9 +15,9 @@ import org.kohsuke.github.GHObject;
 
 import java.io.IOException;
 import java.io.Serial;
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 import static com.axonivy.market.constants.EntityConstants.GITHUB_REPO;
 import static com.axonivy.market.constants.EntityConstants.REPOSITORY_ID;
@@ -72,18 +72,18 @@ public class GithubRepo extends GenericIdEntity {
   private String e2eBadgeUrl;
   @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
   @JoinColumn(name = REPOSITORY_ID)
-  private List<WorkflowInformation> workflowInformation;
+  private Set<WorkflowInformation> workflowInformation;
   private Boolean focused;
   @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
   @JoinColumn(name = REPOSITORY_ID)
-  private List<TestStep> testSteps;
+  private Set<TestStep> testSteps;
 
   public static GithubRepo from(GHObject repo, String productId) throws IOException {
     return GithubRepo.builder()
         .productId(productId)
         .htmlUrl(repo.getHtmlUrl().toString())
-        .workflowInformation(new ArrayList<>())
-        .testSteps(new ArrayList<>())
+        .workflowInformation(new HashSet<>())
+        .testSteps(new HashSet<>())
         .build();
   }
 }
