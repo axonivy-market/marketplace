a97f2513374c97af5d99306a0073ffa8e37f549c
Trần Vĩnh Thiện Phúc <phuc.tranvinhthien@axonactive.com>
2025-10-22 16:46:56 +0700

    MARP-3047 Update UT

--- marketplace-ui/src/app/modules/monitor/monitor-dashboard/monitor-dashboard.component.spec.ts
+++ marketplace-ui/src/app/modules/monitor/monitor-dashboard/monitor-dashboard.component.spec.ts
@@ -1,71 +1,86 @@
-// import { TestBed } from '@angular/core/testing';
-// import { MonitoringDashboardComponent } from './monitor-dashboard.component';
-// import { ActivatedRoute } from '@angular/router';
-// import { of } from 'rxjs';
-// import { PageTitleService } from '../../../shared/services/page-title.service';
-// import {
-//   FOCUSED_TAB,
-//   STANDARD_TAB
-// } from '../../../shared/constants/common.constant';
-// import { PLATFORM_ID } from '@angular/core';
-// import { TranslateService } from '@ngx-translate/core';
-// import { HttpClientTestingModule } from '@angular/common/http/testing';
-//
-// describe('MonitoringDashboardComponent', () => {
-//   let component: MonitoringDashboardComponent;
-//   let pageTitleServiceSpy: jasmine.SpyObj<PageTitleService>;
-//   let translateService: jasmine.SpyObj<TranslateService>;
-//
-//   beforeEach(() => {
-//     pageTitleServiceSpy = jasmine.createSpyObj('PageTitleService', [
-//       'setTitleOnLangChange'
-//     ]);
-//   });
-//
-//   function setup(platformId: any, queryParams: any) {
-//     TestBed.configureTestingModule({
-//       imports: [
-//         MonitoringDashboardComponent,
-//         HttpClientTestingModule, // <-- Add this!
-//       ],
-//       providers: [
-//         { provide: PLATFORM_ID, useValue: platformId },
-//         { provide: ActivatedRoute, useValue: { queryParams: of(queryParams) } },
-//         { provide: PageTitleService, useValue: pageTitleServiceSpy },
-//         { provide: TranslateService, useValue: jasmine.createSpyObj('TranslateService', ['get', 'instant', 'onLangChange']) }
-//       ]
-//     });
-//     component = TestBed.createComponent(MonitoringDashboardComponent).componentInstance;
-//   }
-//
-//   it('should set initialFilter and activeTab from queryParams on browser', () => {
-//     setup('browser', { search: 'test-search' });
-//     component.ngOnInit();
-//
-//     expect(component.initialFilter()).toBe('test-search');
-//     expect(component.activeTab).toBe(STANDARD_TAB);
-//     expect(pageTitleServiceSpy.setTitleOnLangChange).toHaveBeenCalledWith(
-//       'common.monitor.dashboard.pageTitle'
-//     );
-//   });
-//
-//   it('should not set initialFilter if search param is absent', () => {
-//     setup('browser', {});
-//     component.ngOnInit();
-//
-//     expect(component.initialFilter()).toBe('');
-//     expect(component.activeTab).toBe(component.FOCUSED_TAB); // unchanged
-//     expect(pageTitleServiceSpy.setTitleOnLangChange).toHaveBeenCalledWith(
-//       'common.monitor.dashboard.pageTitle'
-//     );
-//   });
-//
-//   it('should set isLoading to false when not in browser', () => {
-//     setup('server', {});
-//     component.isLoading = true;
-//     component.ngOnInit();
-//
-//     expect(component.isLoading).toBe(false);
-//     expect(pageTitleServiceSpy.setTitleOnLangChange).not.toHaveBeenCalled();
-//   });
-// });
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MonitoringDashboardComponent } from './monitor-dashboard.component';
+import { ActivatedRoute, RouterModule } from '@angular/router';
+import { of } from 'rxjs';
+import { PLATFORM_ID } from '@angular/core';
+import { PageTitleService } from '../../../shared/services/page-title.service';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import {
+  provideHttpClient,
+  withInterceptorsFromDi
+} from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { MatomoTestingModule } from 'ngx-matomo-client/testing';
+class MockPageTitleService {
+  setTitleOnLangChange = jasmine.createSpy('setTitleOnLangChange');
+}
+
+describe('MonitoringDashboardComponent', () => {
+  let component: MonitoringDashboardComponent;
+  let fixture: ComponentFixture<MonitoringDashboardComponent>;
+  let mockPageTitleService: MockPageTitleService;
+
+  beforeEach(async () => {
+    mockPageTitleService = new MockPageTitleService();
+
+    await TestBed.configureTestingModule({
+      imports: [
+        MonitoringDashboardComponent,
+        TranslateModule.forRoot(),
+        RouterModule.forRoot([]),
+        MatomoTestingModule.forRoot()
+      ],
+      providers: [
+        provideHttpClient(withInterceptorsFromDi()),
+        provideHttpClientTesting(),
+        { provide: PLATFORM_ID, useValue: 'browser' },
+        { provide: PageTitleService, useValue: mockPageTitleService },
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            queryParams: of({})
+          }
+        },
+        TranslateService
+      ]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(MonitoringDashboardComponent);
+    component = fixture.componentInstance;
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  describe('ngOnInit (platform: browser)', () => {
+    it('should call setTitleOnLangChange', () => {
+      spyOn(component['route'].queryParams, 'subscribe').and.callThrough();
+
+      component.ngOnInit();
+
+      expect(mockPageTitleService.setTitleOnLangChange).toHaveBeenCalledWith(
+        'common.monitor.dashboard.pageTitle'
+      );
+      expect(component['route'].queryParams.subscribe).toHaveBeenCalled();
+    });
+
+    it('should set initialFilter and activeTab when query param "search" exists', () => {
+      const route = TestBed.inject(ActivatedRoute);
+      (route as any).queryParams = of({ search: 'test-repo' });
+
+      component.ngOnInit();
+
+      expect(component.initialFilter()).toBe('test-repo');
+      expect(component.activeTab).toBe(component.STANDARD_TAB);
+    });
+  });
+
+  it('should change active tab when setActiveTab() is called', () => {
+    component.activeTab = component.FOCUSED_TAB;
+
+    component.setActiveTab(component.STANDARD_TAB);
+
+    expect(component.activeTab).toBe(component.STANDARD_TAB);
+  });
+});
