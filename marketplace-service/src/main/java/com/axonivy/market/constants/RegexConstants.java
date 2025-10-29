110503c547d19f89dad2b390074e68b07e935005
quanpham-axonivy <quan.phamvan@axonactive.com>
2025-10-29 18:22:39 +0700

    update regex

--- marketplace-service/src/main/java/com/axonivy/market/constants/RegexConstants.java
+++ marketplace-service/src/main/java/com/axonivy/market/constants/RegexConstants.java
@@ -8,7 +8,7 @@ import java.util.regex.Pattern;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class RegexConstants {
 
-  public static final String SAFE_STRING_REGEX = "^[A-Za-z0-9._\\-]+$";
+  public static final String SAFE_STRING_REGEX = "^[\\p{L}\\p{N}._\\-]+$";
 
   public static final Pattern SAFE_STRING_PATTERN = Pattern.compile(SAFE_STRING_REGEX);
 
