5bbc7164a3a46dacd3f5eda3b34a74c95d42d747
Hoan Nguyen <83745591+nqhoan-axonivy@users.noreply.github.com>
2025-11-13 15:54:16 +0700

    MARP-3138 Read configuration from env file for angular (#483)

--- marketplace-ui/src/environments/environment.development.ts
+++ marketplace-ui/src/environments/environment.development.ts
@@ -1,10 +1,15 @@
+/**
+ * Dear Bug Hunter,
+ * This credential is intentionally included for educational purposes only and does not provide access to any production systems. Please do not submit it as part of our bug bounty program.
+ **/
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:8080',
-  githubClientId: 'Iv23livu9HbsC4Q24eSC',
-  githubAuthCallbackPath: '/auth/github/callback',
+  apiInternalUrl: '',
+  githubOAuthAppClientId: 'Iv23livu9HbsC4Q24eSC',
+  githubOAuthCallback: '/auth/github/callback',
+  githubApiUrl: 'https://api.github.com',
   dayInMiliseconds: 86400000,
-  matomoSiteId: 3,
-  matomoTrackerUrl: '//market.axonivy.com/matomo-web',
-  githubApiUrl: 'https://api.github.com'
+  matomoSiteId: 0,
+  matomoTrackerUrl: '//matomo-web'
 };
