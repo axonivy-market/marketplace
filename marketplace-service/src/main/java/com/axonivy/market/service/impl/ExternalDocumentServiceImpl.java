cef97aa7af15379ba61f596058bc54e880dcc86d
ttan-axonivy <an.truongthien@axonactive.com>
2025-11-14 12:54:04 +0700

    remove unuse import

--- marketplace-service/src/main/java/com/axonivy/market/service/impl/ExternalDocumentServiceImpl.java
+++ marketplace-service/src/main/java/com/axonivy/market/service/impl/ExternalDocumentServiceImpl.java
@@ -44,7 +44,6 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.Optional;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
