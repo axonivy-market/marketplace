795a751f3bb599f12c732997ca5526a7ff81acb7
vhhoang-axonivy <hoang.vuhuy@axonactive.com>
2026-01-29 10:11:42 +0700

    Handle exception

--- marketplace-service/src/main/java/com/axonivy/market/exceptions/MarketExceptionHandler.java
+++ marketplace-service/src/main/java/com/axonivy/market/exceptions/MarketExceptionHandler.java
@@ -5,15 +5,32 @@ import com.axonivy.market.exceptions.model.*;
 import com.axonivy.market.model.Message;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
 @RestControllerAdvice
 public class MarketExceptionHandler {
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public ResponseEntity<Map<String, Object>> handleValidationExceptions(
+      MethodArgumentNotValidException exception) {
+    Map<String, Object> errors = new HashMap<>();
+    errors.put("status", HttpStatus.BAD_REQUEST.value());
+
+    Map<String, String> fieldErrors = new HashMap<>();
+    exception.getBindingResult().getFieldErrors().forEach(error ->
+        fieldErrors.put(error.getField(), error.getDefaultMessage())
+    );
+
+    errors.put("errors", fieldErrors);
+    return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+  }
+
   @ExceptionHandler(AlreadyExistedException.class)
   public ResponseEntity<Object> handleMarketException(AlreadyExistedException alreadyExistedException) {
     var errorMessage = new Message();
