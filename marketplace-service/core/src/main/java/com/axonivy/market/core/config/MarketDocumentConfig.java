1c768ed216bb25e52416e7746505bb83eaca8d38
Dinh Nguyen <dinh.nguyentrinhquoc@axonactive.com>
2026-02-23 23:36:38 +0700

    fix failed test case

--- marketplace-service/core/src/main/java/com/axonivy/market/core/config/MarketDocumentConfig.java
+++ marketplace-service/core/src/main/java/com/axonivy/market/core/config/MarketDocumentConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class MarketApiDocumentConfig {
+public class MarketDocumentConfig {
 
   @Value("${market.info.title}")
   private String title;
