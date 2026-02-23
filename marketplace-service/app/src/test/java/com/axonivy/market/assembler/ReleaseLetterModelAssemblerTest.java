956d0153b3c7b7c6c34e5b4ee06395c4fe05b32c
vhhoang-axonivy <hoang.vuhuy@axonactive.com>
2026-02-23 15:23:01 +0700

    Add tests for release model assembler

--- marketplace-service/app/src/test/java/com/axonivy/market/assembler/ReleaseLetterModelAssemblerTest.java
+++ marketplace-service/app/src/test/java/com/axonivy/market/assembler/ReleaseLetterModelAssemblerTest.java
@@ -1,10 +1,18 @@
 package com.axonivy.market.assembler;
 
+import com.axonivy.market.entity.ReleaseLetter;
+import com.axonivy.market.model.ReleaseLetterModel;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
 class ReleaseLetterModelAssemblerTest {
@@ -18,6 +26,27 @@ class ReleaseLetterModelAssemblerTest {
 
   @Test
   void testToModel() {
+    Date createdDate = new GregorianCalendar(2025, Calendar.FEBRUARY, 23, 10, 30, 0).getTime();
 
+    ReleaseLetter releaseLetter = new ReleaseLetter();
+    releaseLetter.setContent("Release content");
+    releaseLetter.setSprint("S43");
+    releaseLetter.setLatest(true);
+    releaseLetter.setCreatedAt(createdDate);
+
+    ReleaseLetterModel result = releaseLetterModelAssembler.toModel(releaseLetter);
+
+    assertNotNull(result, "Mapped ReleaseLetterModel should not be null");
+    assertEquals("Release content", result.getContent(),
+        "Content should be mapped correctly from ReleaseLetter");
+
+    assertEquals("S43", result.getSprint(),
+        "Sprint should be mapped correctly from ReleaseLetter");
+
+    assertTrue(result.isLatest(),
+        "Latest flag should be mapped correctly from ReleaseLetter");
+
+    assertEquals(createdDate, result.getCreatedAt(),
+        "CreatedAt date should be mapped correctly from ReleaseLetter");
   }
 }
