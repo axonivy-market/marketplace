dfdcab1be61bb0f3a6ad411a17cc978690c80ce4
Dinh Nguyen <dinh.nguyentrinhquoc@axonactive.com>
2026-02-23 23:11:07 +0700

    Update RequestMappingConstants.java

--- marketplace-service/stable/src/main/java/com/axonivy/market/stable/constants/RequestMappingConstants.java
+++ marketplace-service/stable/src/main/java/com/axonivy/market/stable/constants/RequestMappingConstants.java
@@ -3,10 +3,10 @@ package com.axonivy.market.stable.constants;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
+import static com.axonivy.market.core.constants.CoreRequestMappingConstants.API;
+
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class RequestMappingConstants {
-  public static final String ROOT = "/";
-  public static final String API = ROOT + "api";
   public static final String PRODUCT = API + "/product";
   public static final String VERSIONS_BY_ID = "/{id}/versions";
   public static final String PRODUCT_JSON_CONTENT_BY_ID_AND_VERSION = "/{id}/install";
