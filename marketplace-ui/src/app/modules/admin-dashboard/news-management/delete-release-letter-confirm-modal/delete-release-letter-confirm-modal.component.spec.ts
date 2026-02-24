c56095ee1748e93e8deb561b906e3dc2e170b57e
vhhoang-axonivy <hoang.vuhuy@axonactive.com>
2026-02-24 16:59:03 +0700

    Add tests for DeleteReleaseLetterConfirmModalComponent

--- marketplace-ui/src/app/modules/admin-dashboard/news-management/delete-release-letter-confirm-modal/delete-release-letter-confirm-modal.component.spec.ts
+++ marketplace-ui/src/app/modules/admin-dashboard/news-management/delete-release-letter-confirm-modal/delete-release-letter-confirm-modal.component.spec.ts
@@ -1,16 +1,42 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DeleteReleaseLetterConfirmModalComponent } from './delete-release-letter-confirm-modal.component';
+import { AdminDashboardService } from '../../admin-dashboard.service';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { LanguageService } from '../../../../core/services/language/language.service';
+import { ThemeService } from '../../../../core/services/theme/theme.service';
+import { of, Subject } from 'rxjs';
 
 describe('DeleteReleaseLetterConfirmModalComponent', () => {
   let component: DeleteReleaseLetterConfirmModalComponent;
   let fixture: ComponentFixture<DeleteReleaseLetterConfirmModalComponent>;
 
+  let adminDashboardServiceMock: jasmine.SpyObj<AdminDashboardService>;
+  let activeModalMock: jasmine.SpyObj<NgbActiveModal>;
+
   beforeEach(async () => {
+    adminDashboardServiceMock = jasmine.createSpyObj('AdminDashboardService', [
+      'deleteReleaseLetterBySprint'
+    ]);
+
+    activeModalMock = jasmine.createSpyObj('NgbActiveModal', [
+      'close',
+      'dismiss'
+    ]);
+
     await TestBed.configureTestingModule({
-      imports: [DeleteReleaseLetterConfirmModalComponent]
-    })
-    .compileComponents();
+      imports: [
+        DeleteReleaseLetterConfirmModalComponent,
+        TranslateModule.forRoot()
+      ],
+      providers: [
+        { provide: AdminDashboardService, useValue: adminDashboardServiceMock },
+        { provide: NgbActiveModal, useValue: activeModalMock },
+        TranslateService,
+        ThemeService
+      ]
+    }).compileComponents();
 
     fixture = TestBed.createComponent(DeleteReleaseLetterConfirmModalComponent);
     component = fixture.componentInstance;
@@ -19,5 +45,39 @@ describe('DeleteReleaseLetterConfirmModalComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call deleteReleaseLetterBySprint and close modal on success', () => {
+    component.releaseLetterSprint = 'S42';
+
+    adminDashboardServiceMock.deleteReleaseLetterBySprint.and.returnValue(
+      of(void 0)
+    );
+
+    component.deleteReleaseLetterBySprint();
+
+    expect(
+      adminDashboardServiceMock.deleteReleaseLetterBySprint
+    ).toHaveBeenCalledWith('S42');
+
+    expect(activeModalMock.close).toHaveBeenCalled();
+  });
+
+  fit('should not close modal before observable emits', () => {
+    component.releaseLetterSprint = 'S42';
+
+    const subject = new Subject<void>();
+    adminDashboardServiceMock.deleteReleaseLetterBySprint.and.returnValue(
+      subject.asObservable()
+    );
+
+    component.deleteReleaseLetterBySprint();
+
+    expect(activeModalMock.close).not.toHaveBeenCalled();
+
+    subject.next();
+    subject.complete();
+
+    expect(activeModalMock.close).toHaveBeenCalled();
   });
 });
