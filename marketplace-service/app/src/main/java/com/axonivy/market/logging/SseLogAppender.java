9ad8b1c39d11edc99d99f7a1946d5fc3f44a502b
Dinh Nguyen <dinh.nguyentrinhquoc@axonactive.com>
2026-02-26 16:24:33 +0700

    Update SseLogAppender.java

--- marketplace-service/app/src/main/java/com/axonivy/market/logging/SseLogAppender.java
+++ marketplace-service/app/src/main/java/com/axonivy/market/logging/SseLogAppender.java
@@ -3,17 +3,29 @@ package com.axonivy.market.logging;
 import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.core.AppenderBase;
 import ch.qos.logback.core.encoder.Encoder;
+import ch.qos.logback.classic.encoder.PatternLayoutEncoder;
 
 public class SseLogAppender extends AppenderBase<ILoggingEvent> {
   private Encoder<ILoggingEvent> encoder;
 
   @Override
   public void start() {
+    if (encoder == null) {
+      addWarn("No encoder configured for SSE_APPENDER. Initializing default encoder.");
+      // encoder = createDefaultEncoder();
+    }
     if (encoder != null) {
-      encoder.init(null);
+      encoder.start();
     }
     super.start();
   }
+
+  // private Encoder<ILoggingEvent> createDefaultEncoder() {
+  //   PatternLayoutEncoder defaultEncoder = new PatternLayoutEncoder();
+  //   defaultEncoder.setContext(getContext());
+  //   defaultEncoder.setPattern("%d{yyyy-MM-dd HH:mm:ss} %-5level %logger{36} - %msg%n");
+  //   return defaultEncoder;
+  // }
 
   @Override
   protected void append(ILoggingEvent event) {
