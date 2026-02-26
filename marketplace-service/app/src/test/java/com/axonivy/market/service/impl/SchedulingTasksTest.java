481c129f2bf9a9a127cd20a77b7fc67d0442f020
vhhoang-axonivy <hoang.vuhuy@axonactive.com>
2026-02-26 11:00:30 +0700

    Test

--- marketplace-service/app/src/test/java/com/axonivy/market/service/impl/SchedulingTasksTest.java
+++ marketplace-service/app/src/test/java/com/axonivy/market/service/impl/SchedulingTasksTest.java
@@ -1,15 +1,29 @@
 package com.axonivy.market.service.impl;
 
+import com.axonivy.market.constants.GitHubConstants;
+import com.axonivy.market.factory.DisabledSecurityEventFactory;
+import com.axonivy.market.github.model.DisabledSecurityEvent;
+import com.axonivy.market.github.model.GitHubProperty;
+import com.axonivy.market.github.model.ProductSecurityInfo;
+import com.axonivy.market.github.service.GitHubService;
 import com.axonivy.market.schedulingtask.ScheduledTasks;
+import com.axonivy.market.service.NotificationService;
 import org.awaitility.Awaitility;
 import org.awaitility.Durations;
 import org.junit.jupiter.api.Test;
+
+import org.mockito.MockedStatic;
+
+import org.mockito.Mockito;
+
+import static org.mockito.Mockito.*;
+
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.test.context.TestPropertySource;
 
-import static org.mockito.Mockito.atLeast;
-import static org.mockito.Mockito.verify;
+import java.util.List;
 
 @TestPropertySource("classpath:application-test.properties")
 @SpringBootTest
@@ -17,6 +31,15 @@ class SchedulingTasksTest {
 
   @SpyBean
   ScheduledTasks tasks;
+
+  @MockBean
+  GitHubService gitHubService;
+
+  @MockBean
+  NotificationService notificationService;
+
+  @MockBean
+  GitHubProperty gitHubProperty;
 
   @Test
   void testShouldNotTriggerAfterApplicationStarted() {
@@ -36,4 +59,28 @@ class SchedulingTasksTest {
         .untilAsserted(() -> verify(tasks, atLeast(0)).syncDataForSecurityMonitor());
   }
 
+  @Test
+  void shouldSendNotificationWhenSecurityChecksAreDisabled() throws Exception {
+    String token = "dummy-token";
+    when(gitHubProperty.getToken()).thenReturn(token);
+
+    ProductSecurityInfo securityInfo = mock(ProductSecurityInfo.class);
+    when(gitHubService.getSecurityDetailsForAllProducts(
+        eq(token),
+        eq(GitHubConstants.AXONIVY_MARKET_ORGANIZATION_NAME)
+    )).thenReturn(List.of(securityInfo));
+
+    DisabledSecurityEvent event = mock(DisabledSecurityEvent.class);
+
+    try (MockedStatic<DisabledSecurityEventFactory> mockedFactory =
+             Mockito.mockStatic(DisabledSecurityEventFactory.class)) {
+
+      mockedFactory
+          .when(() -> DisabledSecurityEventFactory.from(securityInfo))
+          .thenReturn(List.of(event));
+
+      tasks.syncDataForSecurityMonitor();
+      verify(notificationService).notify(List.of(event));
+    }
+  }
 }
