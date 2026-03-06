a3436beb5929852a703721e28f658d84d7a437f4
vhhoang-axonivy <hoang.vuhuy@axonactive.com>
2026-03-06 10:48:13 +0700

    Clean up

--- marketplace-service/app/src/test/java/com/axonivy/market/rest/axonivy/AxonIvyClientTest.java
+++ marketplace-service/app/src/test/java/com/axonivy/market/rest/axonivy/AxonIvyClientTest.java
@@ -1,25 +1,13 @@
 package com.axonivy.market.rest.axonivy;
 
-import com.axonivy.market.model.DocumentInfoResponse;
-
-import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
-
-import static org.mockito.Mockito.when;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.test.context.TestPropertySource;
 
-import java.util.Collections;
 import java.util.List;
 
-import org.springframework.web.client.RestClientException;
-import org.springframework.web.client.RestTemplate;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 @SpringBootTest
 @TestPropertySource("classpath:application-test.properties")
