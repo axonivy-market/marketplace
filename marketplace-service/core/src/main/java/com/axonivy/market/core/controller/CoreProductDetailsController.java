875bc6f287617954aaeecc743cd435e5a4294a08
vhhoang-axonivy <hoang.vuhuy@axonactive.com>
2026-03-10 15:34:26 +0700

    Clean up

--- marketplace-service/core/src/main/java/com/axonivy/market/core/controller/CoreProductDetailsController.java
+++ marketplace-service/core/src/main/java/com/axonivy/market/core/controller/CoreProductDetailsController.java
@@ -1,7 +1,10 @@
 package com.axonivy.market.core.controller;
 
+import com.axonivy.market.core.service.CoreVersionService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,22 +17,19 @@ import static com.axonivy.market.core.constants.CoreRequestParamConstants.*;
 
 @AllArgsConstructor
 @Tag(name = "Product Detail Controllers", description = "API collection to get product's detail.")
-public abstract class CoreProductDetailsController {
+public class CoreProductDetailsController {
+  private final CoreVersionService coreVersionService;
 
-    @GetMapping(PRODUCT_JSON_CONTENT_BY_PRODUCT_ID_AND_VERSION)
-    public ResponseEntity<Map<String, Object>> findProductJsonContent(
-            @PathVariable(ID) String productId,
-            @PathVariable(VERSION) String version,
-            @RequestParam(name = DESIGNER_VERSION, required = false) String designerVersion) {
+  @GetMapping(PRODUCT_JSON_CONTENT_BY_PRODUCT_ID_AND_VERSION)
+  @Operation(summary = "Get product json content for designer to install",
+      description = "When we click install in designer, this API will send content of product json for installing in " +
+          "Ivy designer")
+  public ResponseEntity<Map<String, Object>> findProductJsonContent(@PathVariable(ID) String productId,
+      @PathVariable(VERSION) String version,
+      @RequestParam(name = DESIGNER_VERSION, required = false) String designerVersion) {
+    Map<String, Object> productJsonContent = coreVersionService.getProductJsonContentByIdAndVersion(productId,
+        designerVersion);
+    return new ResponseEntity<>(productJsonContent, HttpStatus.OK);
+  }
 
-        Map<String, Object> productJsonContent =
-                getProductJsonContent(productId, version, designerVersion);
-
-        return ResponseEntity.ok(productJsonContent);
-    }
-
-    protected abstract Map<String, Object> getProductJsonContent(
-            String productId,
-            String version,
-            String designerVersion);
 }
