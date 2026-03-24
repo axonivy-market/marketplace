692ae0334ab5c29fdc5288b8e3ed3817f1c7cb7d
Dinh Nguyen <dinh.nguyentrinhquoc@axonactive.com>
2026-03-24 13:24:18 +0700

    Revert "debug"
    This reverts commit f29cc3cf1150e7a23377592bb5144c0009928b05.

--- marketplace-ui/src/app/modules/product/product-detail/product-detail-version-action/product-detail-version-action.component.ts
+++ marketplace-ui/src/app/modules/product/product-detail/product-detail-version-action/product-detail-version-action.component.ts
@@ -287,12 +287,12 @@ export class ProductDetailVersionActionComponent implements AfterViewInit {
     this.isDownloading.set(true);
     const version = this.selectedVersion().replace(VERSION.displayPrefix, '');
     if (!this.isCheckedAppForEngine || this.selectedArtifactId?.endsWith(DOC) || this.selectedArtifact?.endsWith(ZIP)) {
-      downloadUrl = `${environment.apiUrl}/${API_URI.PRODUCT_MARKETPLACE_DATA}/${this.productId}/${this.selectedArtifactId}/${version}`;
+      downloadUrl = `${this.getMarketplaceServiceUrl()}/${API_URI.PRODUCT_MARKETPLACE_DATA}/${this.productId}/${this.selectedArtifactId}/${version}`;
       if (this.selectedArtifact) {
         this.fetchAndDownloadArtifact(downloadUrl, this.selectedArtifact.substring(this.selectedArtifact.lastIndexOf('/') + 1));
       }
     } else if (this.isCheckedAppForEngine) {
-      downloadUrl = `${environment.apiUrl}/${API_URI.PRODUCT_DETAILS}/${this.productId}/${this.selectedArtifactId}/${version}/zip-file`;
+      downloadUrl = `${this.getMarketplaceServiceUrl()}/${API_URI.PRODUCT_DETAILS}/${this.productId}/${this.selectedArtifactId}/${version}/zip-file`;
       this.fetchAndDownloadArtifact(downloadUrl, `${this.selectedArtifactId}-app-${version}.zip`);
     } else {
       return;
