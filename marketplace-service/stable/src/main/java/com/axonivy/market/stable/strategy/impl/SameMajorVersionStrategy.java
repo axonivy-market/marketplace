1a35c89a77b936f955b4f4483f9573e0553ce213
vhhoang-axonivy <hoang.vuhuy@axonactive.com>
2026-03-31 17:59:06 +0700

    Refactor find best match version

--- marketplace-service/stable/src/main/java/com/axonivy/market/stable/strategy/impl/SameMajorVersionStrategy.java
+++ marketplace-service/stable/src/main/java/com/axonivy/market/stable/strategy/impl/SameMajorVersionStrategy.java
@@ -1,5 +1,7 @@
 package com.axonivy.market.stable.strategy.impl;
 
+import com.axonivy.market.core.enums.ErrorCode;
+import com.axonivy.market.core.exceptions.model.NotFoundException;
 import com.axonivy.market.core.strategy.VersionMatchStrategy;
 import org.springframework.util.CollectionUtils;
 
@@ -9,10 +11,19 @@ public class SameMajorVersionStrategy implements VersionMatchStrategy {
   @Override
   public String findMatch(List<String> versions, String requestedVersion) {
     if (CollectionUtils.isEmpty(versions)) {
-      return requestedVersion;
+      throw new NotFoundException(ErrorCode.VERSION_NOT_FOUND, "Cannot found version: " + requestedVersion);
     }
 
     String targetMajor = requestedVersion.split("\\.")[0];
+
+//    return versions.stream()
+//        .filter(v -> {
+//          if (v == null) return false;
+//          String[] parts = v.split("\\.");
+//          return parts.length > 0 && parts[0].equals(targetMajor);
+//        })
+//        .findFirst()
+//        .orElse(versions.get(0));
 
     return versions.stream()
         .filter(v -> {
@@ -21,6 +32,9 @@ public class SameMajorVersionStrategy implements VersionMatchStrategy {
           return parts.length > 0 && parts[0].equals(targetMajor);
         })
         .findFirst()
-        .orElse(versions.get(0));
+        .orElseThrow(() -> new NotFoundException(
+            ErrorCode.VERSION_NOT_FOUND,
+            "No version found for major: " + targetMajor
+        ));
   }
 }
