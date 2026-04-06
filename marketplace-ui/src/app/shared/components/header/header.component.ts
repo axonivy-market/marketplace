d2679bb949012ffcb226c7d068644285ce77e731
vhhoang <hoang.vuhuy@axonactive.com>
2026-04-06 18:04:19 +0700

    Fix header

--- marketplace-ui/src/app/shared/components/header/header.component.ts
+++ marketplace-ui/src/app/shared/components/header/header.component.ts
@@ -1,5 +1,15 @@
 import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, Input, Output, inject, model } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+  TemplateRef,
+  WritableSignal,
+  inject,
+  model,
+  signal
+} from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { NavigationEnd, Router, RouterLink } from '@angular/router';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
@@ -9,6 +19,12 @@ import { NavigationComponent } from './navigation/navigation.component';
 import { SearchBarComponent } from './search-bar/search-bar.component';
 import { filter } from 'rxjs';
 import { GithubUserBadgeComponent } from '../github-user-badge/github-user-badge.component';
+import {
+  NgbDatepicker,
+  NgbInputDatepicker,
+  NgbOffcanvas,
+  OffcanvasDismissReasons
+} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-header',
@@ -19,15 +35,21 @@ import { GithubUserBadgeComponent } from '../github-user-badge/github-user-badge
     NavigationComponent,
     SearchBarComponent,
     RouterLink,
-    GithubUserBadgeComponent
+    GithubUserBadgeComponent,
+    NgbInputDatepicker
   ],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss', '../../../app.component.scss']
 })
 export class HeaderComponent {
+  private offcanvasService = inject(NgbOffcanvas);
+  closeResult: WritableSignal<string> = signal('');
+
   selectedNav = '/';
 
   isMobileMenuCollapsed = model<boolean>(true);
+
+  isSidebarOpen = model<boolean>(false);
 
   themeService = inject(ThemeService);
   translateService = inject(TranslateService);
@@ -48,11 +70,42 @@ export class HeaderComponent {
 
     this.updateAdminState(this.router.url);
     this.router.events
-      .pipe(filter((event): event is NavigationEnd => event instanceof NavigationEnd))
+      .pipe(
+        filter(
+          (event): event is NavigationEnd => event instanceof NavigationEnd
+        )
+      )
       .subscribe(event => {
         const url = event.urlAfterRedirects ?? event.url;
         this.updateAdminState(url);
       });
+  }
+
+  open(content: TemplateRef<any>) {
+    this.offcanvasService
+      .open(content, {
+        ariaLabelledBy: 'offcanvas-basic-title',
+        backdrop: false
+      })
+      .result.then(
+        result => {
+          this.closeResult.set(`Closed with: ${result}`);
+        },
+        reason => {
+          this.closeResult.set(`Dismissed ${this.getDismissReason(reason)}`);
+        }
+      );
+  }
+
+  private getDismissReason(reason: any): string {
+    switch (reason) {
+      case OffcanvasDismissReasons.ESC:
+        return 'by pressing ESC';
+      case OffcanvasDismissReasons.BACKDROP_CLICK:
+        return 'by clicking on the backdrop';
+      default:
+        return `with: ${reason}`;
+    }
   }
 
   onCollapsedMobileMenu() {
@@ -65,5 +118,9 @@ export class HeaderComponent {
 
   private updateAdminState(url: string): void {
     this.isAdminRoute = url.startsWith('/internal-dashboard');
+  }
+
+  openSideBar() {
+    this.isSidebarOpen.update(value => !value);
   }
 }
