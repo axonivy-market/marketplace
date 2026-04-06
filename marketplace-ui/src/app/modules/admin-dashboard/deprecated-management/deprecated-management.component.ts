63ccbcb610bffd4311c24088c3c05e56192c224a
Trần Vĩnh Thiện Phúc <phuc.tranvinhthien@axonactive.com>
2026-04-06 16:04:40 +0700

    fix

--- marketplace-ui/src/app/modules/admin-dashboard/deprecated-management/deprecated-management.component.ts
+++ marketplace-ui/src/app/modules/admin-dashboard/deprecated-management/deprecated-management.component.ts
@@ -96,7 +96,7 @@ export class DeprecatedManagementComponent implements OnInit {
   }
 
   private initializeDeprecatedRows(): void {
-    this.refreshDeprecatedRows().then(data => {});
+    this.refreshDeprecatedRows().then((_data) => {});
   }
 
   trigger() {
