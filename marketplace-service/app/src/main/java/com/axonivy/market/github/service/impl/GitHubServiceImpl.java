e1a61cd2a9497408a247fdce92e3e6a386b026e6
Trần Vĩnh Thiện Phúc <phuc.tranvinhthien@axonactive.com>
2026-04-15 16:56:49 +0700

    MARP-3842 Handle Feedbacks

--- marketplace-service/app/src/main/java/com/axonivy/market/github/service/impl/GitHubServiceImpl.java
+++ marketplace-service/app/src/main/java/com/axonivy/market/github/service/impl/GitHubServiceImpl.java
@@ -491,6 +491,7 @@ public class GitHubServiceImpl implements GitHubService {
     boolean isSameContent = Objects.equals(currentReadmeContent, pullRequestData.updatedReadmeContent);
     if (isSameContent) {
       log.error("No Need to update readme content for deprecation because the content is the same");
+      removeBranchIfExistsWhenRemoveDeprecation(repository, HEADS_PREFIX + config.unsupportedBranchName, action);
       return null;
     }
 
@@ -529,6 +530,19 @@ public class GitHubServiceImpl implements GitHubService {
       readme.update(pullRequestData.updatedReadmeContent, pullRequestData.body, unsupportedBranchName);
     }
     return generatePullRequest(repository, baseBranch, pullRequestData);
+  }
+
+  private void removeBranchIfExistsWhenRemoveDeprecation(GHRepository repository, String headBranchName,
+      PullRequestAction action) throws IOException {
+    if (action != REMOVE) {
+      return;
+    }
+
+    try {
+      repository.getRef(headBranchName).delete();
+    } catch (GHFileNotFoundException e) {
+      log.info("Branch '{}' does not exist, skipping deletion", headBranchName);
+    }
   }
 
   private String getReadmeContent(GHContent readme) throws IOException {
