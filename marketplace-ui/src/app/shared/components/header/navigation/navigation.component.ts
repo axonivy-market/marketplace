cf56d906ce3edcd79593ac751cc5198a9daa0a0d
vhhoang <hoang.vuhuy@axonactive.com>
2026-04-23 16:56:50 +0700

    Clean up

--- marketplace-ui/src/app/shared/components/header/navigation/navigation.component.ts
+++ marketplace-ui/src/app/shared/components/header/navigation/navigation.component.ts
@@ -1,21 +1,9 @@
 import { CommonModule } from '@angular/common';
-import {
-  Component,
-  HostListener,
-  inject,
-  Input,
-  Renderer2,
-  signal
-} from '@angular/core';
+import { Component, inject, Input, signal } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
-import { WindowRef } from '../../../../core/services/browser/window-ref.service';
 import { LanguageService } from '../../../../core/services/language/language.service';
-import {
-  ADMIN_NAV_ITEMS,
-  NAV_ITEMS,
-  SEARCH_URL
-} from '../../../constants/common.constant';
+import { ADMIN_NAV_ITEMS, NAV_ITEMS, SEARCH_URL } from '../../../constants/common.constant';
 import { NavItem } from '../../../models/nav-item.model';
 import { HeaderOffcanvasService } from '../../../services/header-offcanvas.service';
 
@@ -35,26 +23,6 @@ export class NavigationComponent {
   headerOffcanvasService = inject(HeaderOffcanvasService);
   isMobileMode = signal<boolean>(false);
   searchUrl = SEARCH_URL;
-
-  constructor(
-    private readonly windowRef: WindowRef,
-    private readonly renderer: Renderer2
-  ) {
-    this.checkMediaSize();
-  }
-
-  @HostListener('window:resize')
-  onResize() {
-    this.checkMediaSize();
-  }
-
-  checkMediaSize() {
-    const win = this.windowRef.nativeWindow;
-    if (win) {
-      const mediaQuery = win.matchMedia('(max-width: 992px)');
-      this.isMobileMode.set(mediaQuery.matches);
-    }
-  }
 
   get items(): NavItem[] {
     return this.isAdminPage ? this.adminNavItems : this.navItems;
