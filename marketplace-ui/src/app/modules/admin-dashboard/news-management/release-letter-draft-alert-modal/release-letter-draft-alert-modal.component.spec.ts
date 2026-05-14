3774dc046e8241f71faf1d8abb722d68cfd8840f
vhhoang <hoang.vuhuy@axonactive.com>
2026-05-14 11:36:45 +0700

    Add test

---
+++ marketplace-ui/src/app/modules/admin-dashboard/news-management/release-letter-draft-alert-modal/release-letter-draft-alert-modal.component.spec.ts
@@ -0,0 +1,49 @@
+import { beforeEach, describe, expect, it, MockedObject, vi } from "vitest";
+import { ReleaseLetterDraftAlertModalComponent } from "./release-letter-draft-alert-modal.component";
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { NewsManagementService } from "../news-management.service";
+import { NgbActiveModal } from "@ng-bootstrap/ng-bootstrap";
+import { TranslateModule, TranslateService } from "@ngx-translate/core";
+import { ThemeService } from "../../../../core/services/theme/theme.service";
+
+describe('ReleaseLetterDraftAlertModalComponent', () => {
+  let component: ReleaseLetterDraftAlertModalComponent;
+  let fixture: ComponentFixture<ReleaseLetterDraftAlertModalComponent>;
+  let newsManagementServiceMock: MockedObject<NewsManagementService>;
+  let activeModalMock: MockedObject<NgbActiveModal>;
+
+  beforeEach(async () => {
+    newsManagementServiceMock = {
+      deleteReleaseLetterById: vi
+        .fn()
+        .mockName('NewsManagementService.deleteReleaseLetterById')
+    } as MockedObject<NewsManagementService>;
+
+    activeModalMock = {
+      close: vi.fn().mockName('NgbActiveModal.close'),
+      dismiss: vi.fn().mockName('NgbActiveModal.dismiss'),
+      update: vi.fn().mockName('NgbActiveModal.update')
+    } as MockedObject<NgbActiveModal>;
+
+    await TestBed.configureTestingModule({
+      imports: [
+        ReleaseLetterDraftAlertModalComponent,
+        TranslateModule.forRoot()
+      ],
+      providers: [
+        { provide: NewsManagementService, useValue: newsManagementServiceMock },
+        { provide: NgbActiveModal, useValue: activeModalMock },
+        TranslateService,
+        ThemeService
+      ]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(ReleaseLetterDraftAlertModalComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
