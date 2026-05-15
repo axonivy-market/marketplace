9897bf245144d5da7b260b2f771a09874057b43a
vhhoang <hoang.vuhuy@axonactive.com>
2026-05-15 10:23:46 +0700

    Clean up

--- marketplace-service/app/src/main/java/com/axonivy/market/repository/ReleaseLetterRepository.java
+++ marketplace-service/app/src/main/java/com/axonivy/market/repository/ReleaseLetterRepository.java
@@ -4,14 +4,11 @@ import com.axonivy.market.entity.ReleaseLetter;
 import jakarta.transaction.Transactional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface ReleaseLetterRepository extends JpaRepository<ReleaseLetter, String> {
