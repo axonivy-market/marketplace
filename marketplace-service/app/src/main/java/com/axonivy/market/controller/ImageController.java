31423a248b98344f8d400c7c09a81faa8ca4a07f
Thuy Nguyen <thuy.nguyenngocthanh@axonactive.com>
2026-05-27 17:04:51 +0700

    Fix sonar

--- marketplace-service/app/src/main/java/com/axonivy/market/controller/ImageController.java
+++ marketplace-service/app/src/main/java/com/axonivy/market/controller/ImageController.java
@@ -2,19 +2,10 @@ package com.axonivy.market.controller;
 
 import com.axonivy.market.core.controller.CoreImageController;
 import com.axonivy.market.service.ImageService;
-import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.MediaTypeFactory;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import static com.axonivy.market.constants.RequestMappingConstants.BY_FILE_NAME;
 import static com.axonivy.market.core.constants.CoreRequestMappingConstants.IMAGE;
 
 @RestController
