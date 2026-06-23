c36b37784f80b5b036a5caddecb76f11fe178715
quanpham-axonivy <quan.phamvan@axonactive.com>
2026-06-23 17:22:24 +0700

    clean after merge dev

--- marketplace-service/app/src/test/java/com/axonivy/market/rest/axonivy/AxonIvyClientTest.java
+++ marketplace-service/app/src/test/java/com/axonivy/market/rest/axonivy/AxonIvyClientTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.TestPropertySource;
 
 import java.util.List;
 
--- marketplace-service/app/src/test/java/com/axonivy/market/util/OpenApisUtilsTest.java
+++ marketplace-service/app/src/test/java/com/axonivy/market/util/OpenApisUtilsTest.java
@@ -6,9 +6,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.http.*;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
