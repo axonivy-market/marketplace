1684b9eebb999e74f93207d2352acd36aaf50ce1
Dinh Nguyen <dinh.nguyentrinhquoc@axonactive.com>
2026-07-08 08:40:09 +0700

    refactor

--- marketplace-service/app/src/main/java/com/axonivy/market/controller/AdminSessionAuthController.java
+++ marketplace-service/app/src/main/java/com/axonivy/market/controller/AdminSessionAuthController.java
@@ -10,7 +10,6 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
