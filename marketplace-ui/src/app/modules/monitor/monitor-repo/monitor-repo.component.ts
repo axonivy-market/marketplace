b13a73bdef6b80255abe304c4ad2e89e3b2c31c5
Dinh Nguyen <dinh.nguyentrinhquoc@axonactive.com>
2026-07-15 14:18:49 +0700

    udpate tét case v1

--- marketplace-ui/src/app/modules/monitor/monitor-repo/monitor-repo.component.ts
+++ marketplace-ui/src/app/modules/monitor/monitor-repo/monitor-repo.component.ts
@@ -17,12 +17,7 @@ import { CommonModule } from '@angular/common';
 import { LanguageService } from '../../../core/services/language/language.service';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
 import { BuildBadgeTooltipComponent } from '../build-badge-tooltip/build-badge-tooltip.component';
-import {
-  NgbTooltipModule,
-  NgbPagination,
-  NgbPaginationModule,
-  NgbTypeaheadModule
-} from '@ng-bootstrap/ng-bootstrap';
+import { NgbTooltipModule, NgbPagination, NgbPaginationModule, NgbTypeaheadModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule } from '@angular/forms';
 import { ProductFilterComponent } from '../../product/product-filter/product-filter.component';
 import { RepoTestResultComponent } from '../repo-test-result/repo-test-result.component';
@@ -102,6 +97,7 @@ export class MonitoringRepoComponent implements OnInit, OnDestroy {
   languageService = inject(LanguageService);
   translateService = inject(TranslateService);
   githubService = inject(GithubService);
+  cdr = inject(ChangeDetectorRef);
   router = inject(Router);
   route = inject(ActivatedRoute);
   platformId = inject(PLATFORM_ID);
