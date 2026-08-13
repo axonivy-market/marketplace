89a378bb08193807b8d2dca67a1e7d5290c42e3c
Loc Nguyen <loc.nguyenthanh@axonactive.com>
2026-08-13 11:12:00 +0700

    refactor: test data file

--- marketplace-service/core/src/test/java/com/axonivy/market/core/CoreTestApplication.java
+++ marketplace-service/core/src/test/java/com/axonivy/market/core/CoreTestApplication.java
@@ -1,11 +1,9 @@
 package com.axonivy.market.core;
 
-import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.AutoConfigurationPackage;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootConfiguration
 @EnableAutoConfiguration
 @AutoConfigurationPackage
 @EnableJpaRepositories(basePackages = "com.axonivy.market.core.repository")
